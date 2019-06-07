d6e1b7f8e823ec9df616505186d7a39dc1ba4773
Valentyn Kolesnikov <javadev75@gmail.com>
2019-06-07 14:18:11 +0300

    Add "5. Longest Palindromic Substring" solution.

---
+++ src/test/kotlin/s0005/longest/palindromic/substring/SolutionTest.kt
@@ -0,0 +1,12 @@
+package s0005.longest.palindromic.substring
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun findMedianSortedArrays() {
+        assertThat(Solution().longestPalindrome("babad"), equalTo("aba"))
+    }
+}
