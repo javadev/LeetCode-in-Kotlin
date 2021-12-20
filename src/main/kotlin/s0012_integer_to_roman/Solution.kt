3c6b68ff4b40057cf19e006b2cfdd3ce59b7d218
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-20 20:00:49 +0200

    Added task 12.

---
+++ src/main/kotlin/s0012_integer_to_roman/Solution.kt
@@ -0,0 +1,60 @@
+package s0012_integer_to_roman
+
+class Solution {
+    fun intToRoman(num: Int): String? {
+        var num = num
+        val sb = StringBuilder()
+        val m = 1000
+        val c = 100
+        val x = 10
+        val i = 1
+        num = numerals(sb, num, m, ' ', ' ', 'M')
+        num = numerals(sb, num, c, 'M', 'D', 'C')
+        num = numerals(sb, num, x, 'C', 'L', 'X')
+        numerals(sb, num, i, 'X', 'V', 'I')
+        return sb.toString()
+    }
+
+    private fun numerals(sb: StringBuilder, num: Int, one: Int, cTen: Char, cFive: Char, cOne: Char): Int {
+        val div = num / one
+        when (div) {
+            9 -> {
+                sb.append(cOne)
+                sb.append(cTen)
+            }
+            8 -> {
+                sb.append(cFive)
+                sb.append(cOne)
+                sb.append(cOne)
+                sb.append(cOne)
+            }
+            7 -> {
+                sb.append(cFive)
+                sb.append(cOne)
+                sb.append(cOne)
+            }
+            6 -> {
+                sb.append(cFive)
+                sb.append(cOne)
+            }
+            5 -> sb.append(cFive)
+            4 -> {
+                sb.append(cOne)
+                sb.append(cFive)
+            }
+            3 -> {
+                sb.append(cOne)
+                sb.append(cOne)
+                sb.append(cOne)
+            }
+            2 -> {
+                sb.append(cOne)
+                sb.append(cOne)
+            }
+            1 -> sb.append(cOne)
+            else -> {
+            }
+        }
+        return num - div * one
+    }
+}
