7e59bd751ea20e4d0f1e073c635f4a594677a982
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-21 16:53:10 +0200

    Added task 864.

---
+++ src/test/kotlin/g0801_0900/s0864_shortest_path_to_get_all_keys/SolutionTest.kt
@@ -0,0 +1,20 @@
+package g0801_0900.s0864_shortest_path_to_get_all_keys
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+
+import org.junit.Test
+
+internal class SolutionTest {
+    @Test
+    fun shortestPathAllKeys() {
+        val solution = Solution()
+        assertThat(
+            solution.shortestPathAllKeys(arrayOf("@.a.#", "###.#", "b.A.B")), equalTo(8)
+        )
+        assertThat(
+            solution.shortestPathAllKeys(arrayOf("@..aA", "..B#.", "....b")), equalTo(6)
+        )
+        assertThat(solution.shortestPathAllKeys(arrayOf("@Aa")), equalTo(-1))
+    }
+}
