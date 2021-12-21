472e208878f6350b3275aedfd4f9bd59812f36df
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-21 21:10:10 +0200

    Improved task 10.

--- src/main/kotlin/g0001_0100/s0010_regular_expression_matching/Solution.kt
+++ src/main/kotlin/g0001_0100/s0010_regular_expression_matching/Solution.kt
@@ -1,19 +1,28 @@
 package g0001_0100.s0010_regular_expression_matching
 
 class Solution {
-    fun isMatch(text: String, pattern: String): Boolean {
-        val dp = Array(text.length + 1) { BooleanArray(pattern.length + 1) }
-        dp[text.length][pattern.length] = true
-        for (i in text.length downTo 0) {
-            for (j in pattern.length - 1 downTo 0) {
-                val firstMatch = i < text.length && (pattern[j] == text[i] || pattern[j] == '.')
-                if (j + 1 < pattern.length && pattern[j + 1] == '*') {
-                    dp[i][j] = dp[i][j + 2] || firstMatch && dp[i + 1][j]
-                } else {
-                    dp[i][j] = firstMatch && dp[i + 1][j + 1]
-                }
-            }
+    private lateinit var cache: Array<Array<Boolean?>>
+
+    fun isMatch(s: String, p: String): Boolean {
+        cache = Array(s.length + 1) { arrayOfNulls(p.length + 1) }
+        return isMatch(s, p, 0, 0)
+    }
+
+    private fun isMatch(s: String, p: String, i: Int, j: Int): Boolean {
+        if (j == p.length) {
+            return i == s.length
         }
-        return dp[0][0]
+        val result: Boolean
+        if (cache[i][j] != null) {
+            return cache[i][j]!!
+        }
+        val firstMatch = i < s.length && (s[i] == p[j] || p[j] == '.')
+        result = if (j + 1 < p.length && p[j + 1] == '*') {
+            firstMatch && isMatch(s, p, i + 1, j) || isMatch(s, p, i, j + 2)
+        } else {
+            firstMatch && isMatch(s, p, i + 1, j + 1)
+        }
+        cache[i][j] = result
+        return result
     }
 }
