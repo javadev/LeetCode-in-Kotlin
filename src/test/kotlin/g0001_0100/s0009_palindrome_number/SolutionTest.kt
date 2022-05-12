3f65a3b9d5b8834de5400d75fc69361621c8d6da
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-12 20:49:52 +0300

    Added tests 9, 10.

--- src/test/kotlin/g0001_0100/s0009_palindrome_number/SolutionTest.kt
+++ src/test/kotlin/g0001_0100/s0009_palindrome_number/SolutionTest.kt
@@ -2,16 +2,20 @@ package g0001_0100.s0009_palindrome_number
 
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
 
 internal class SolutionTest {
+    @Test
     fun isPalindrome() {
         assertThat(Solution().isPalindrome(121), equalTo(true))
     }
 
+    @Test
     fun isPalindrome2() {
         assertThat(Solution().isPalindrome(-121), equalTo(false))
     }
 
+    @Test
     fun isPalindrome3() {
         assertThat(Solution().isPalindrome(10), equalTo(false))
     }
--- src/test/kotlin/g0001_0100/s0010_regular_expression_matching/SolutionTest.kt
+++ src/test/kotlin/g0001_0100/s0010_regular_expression_matching/SolutionTest.kt
@@ -2,24 +2,30 @@ package g0001_0100.s0010_regular_expression_matching
 
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
 
 internal class SolutionTest {
+    @Test
     fun isMatch() {
         assertThat(Solution().isMatch("aa", "a"), equalTo(false))
     }
 
+    @Test
     fun isMatch2() {
         assertThat(Solution().isMatch("aa", "a*"), equalTo(true))
     }
 
+    @Test
     fun isMatch3() {
         assertThat(Solution().isMatch("ab", ".*"), equalTo(true))
     }
 
+    @Test
     fun isMatch4() {
         assertThat(Solution().isMatch("aab", "c*a*b"), equalTo(true))
     }
 
+    @Test
     fun isMatch5() {
         assertThat(Solution().isMatch("mississippi", "mis*is*p*."), equalTo(false))
     }
