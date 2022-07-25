c8951dbda96db1f6c856aad9fcfb19cec775c14d
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-26 02:27:40 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
     jacoco
     id("org.sonarqube") version "3.3"
     id("com.diffplug.spotless") version "6.1.2"
@@ -14,7 +14,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
     testImplementation("org.junit.jupiter:junit-jupiter-api:[5.8.2,)")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:[5.8.2,)")
     testImplementation("org.hamcrest:hamcrest-core:[2.2,)")
