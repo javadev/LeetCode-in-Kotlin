76f9d8dc578a0e05a757d42d6b27293fffeaf9f1
Valentyn Kolesnikov <javadev75@gmail.com>
2022-09-03 11:41:42 +0300

    Update Solution.kt

--- src/main/kotlin/g0101_0200/s0128_longest_consecutive_sequence/Solution.kt
+++ src/main/kotlin/g0101_0200/s0128_longest_consecutive_sequence/Solution.kt
@@ -10,32 +10,25 @@ class Solution {
         if (nums.isEmpty()) {
             return 0
         }
-
         val queue = PriorityQueue<Int>()
         nums.forEach {
             queue.add(it)
         }
-
         var lastNum = Integer.MIN_VALUE
         var length = 0
         var maxLength = 1
-
         while (!queue.isEmpty()) {
             val num = queue.poll()
             if (num == lastNum) {
                 continue
             }
-
             length ++
-
             if (num - lastNum > 1) {
                 length = 1
             }
-
             lastNum = num
             maxLength = maxOf(maxLength, length)
         }
-
         return maxLength
     }
 }
