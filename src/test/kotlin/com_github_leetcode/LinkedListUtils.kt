f9bb573b7edf57ce08b16d4c2057203a9794aaf0
Valentyn Kolesnikov <javadev75@gmail.com>
2022-09-03 08:17:02 +0300

    Improved ktlint

--- src/test/kotlin/com_github_leetcode/LinkedListUtils.kt
+++ src/test/kotlin/com_github_leetcode/LinkedListUtils.kt
@@ -1,8 +1,8 @@
 package com_github_leetcode
 
 object LinkedListUtils {
-    fun contructLinkedList(nums: IntArray?): ListNode? {
-        if (nums == null || nums.isEmpty()) {
+    fun contructLinkedList(nums: IntArray): ListNode? {
+        if (nums.isEmpty()) {
             return null
         }
         val pre = ListNode(-1)
@@ -15,12 +15,14 @@ object LinkedListUtils {
         return pre.next
     }
 
-    fun createSinglyLinkedList(listValues: List<Int?>?): ListNode {
-        require(!(listValues == null || listValues.isEmpty())) { "Please pass in a valid listValues to create a singly linked list." }
-        val head = ListNode(listValues[0]!!)
+    fun createSinglyLinkedList(listValues: List<Int>): ListNode {
+        require(listValues.isNotEmpty()) {
+            "Please pass in a valid listValues to create a singly linked list."
+        }
+        val head = ListNode(listValues[0])
         var tmp: ListNode? = head
         for (i in 1 until listValues.size) {
-            val next = ListNode(listValues[i]!!)
+            val next = ListNode(listValues[i])
             tmp!!.next = next
             tmp = tmp.next
         }
