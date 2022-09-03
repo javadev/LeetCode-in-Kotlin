ab77603721b2d2a8973214bf0ceeff525a0becde
Valentyn Kolesnikov <javadev75@gmail.com>
2022-09-03 08:22:50 +0300

    Improved ktlint.

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,9 @@ spotless {
     kotlin {
         encoding("UTF-8")
         target("**/*.kt")
-        ktlint("0.43.0")
+        ktlint("0.43.0").userData(mapOf(
+                "max_line_length" to "120"
+                ))
         toggleOffOn()
         trimTrailingWhitespace()
         endWithNewline()
