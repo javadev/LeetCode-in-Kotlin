23c05e2d12a94b2b05c8e5e15a542055b087754a
Valentyn Kolesnikov <javadev75@gmail.com>
2022-09-03 08:33:13 +0300

    Update Solution.kt

--- src/main/kotlin/g0101_0200/s0121_best_time_to_buy_and_sell_stock/Solution.kt
+++ src/main/kotlin/g0101_0200/s0121_best_time_to_buy_and_sell_stock/Solution.kt
@@ -2,7 +2,7 @@ package g0101_0200.s0121_best_time_to_buy_and_sell_stock
 
 // #Easy #Top_100_Liked_Questions #Top_Interview_Questions #Array #Dynamic_Programming
 // #Data_Structure_I_Day_3_Array #Dynamic_Programming_I_Day_7 #Level_1_Day_5_Greedy #Udemy_Arrays
-// #2022_09_03_Time_609_ms_(94.06%)_Space_91.9_MB_(46.38%)
+// #2022_09_03_Time_609_ms_(94.06%)_Space_57.3_MB_(94.43%)
 
 class Solution {
     fun maxProfit(prices: IntArray): Int {
