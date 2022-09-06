e22d3df4777f74b4add27835328622e8a18a64ee
Valentyn Kolesnikov <javadev75@gmail.com>
2022-09-06 19:57:46 +0300

    Update Solution.kt

--- src/main/kotlin/g0101_0200/s0148_sort_list/Solution.kt
+++ src/main/kotlin/g0101_0200/s0148_sort_list/Solution.kt
@@ -2,20 +2,24 @@ package g0101_0200.s0148_sort_list
 
 // #Medium #Top_100_Liked_Questions #Top_Interview_Questions #Sorting #Two_Pointers #Linked_List
 // #Divide_and_Conquer #Merge_Sort #Level_2_Day_4_Linked_List
-// #2022_09_06_Time_495_ms_(91.49%)_Space_44.5_MB_(100.00%)
+// #2022_09_06_Time_820_ms_(61.70%)_Space_76_MB_(28.72%)
 
 import com_github_leetcode.ListNode
 
+/*
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
 class Solution {
-    /**
-     * Credit: https://discuss.leetcode.com/topic/18100/java-merge-sort-solution
-     * But this is not using constant space.
-     */
     fun sortList(head: ListNode?): ListNode? {
         if (head == null || head.next == null) {
             return head
         }
-
         // Step 1: split the list into halves
         var prev: ListNode? = null
         var slow = head
@@ -26,11 +30,9 @@ class Solution {
             slow = slow!!.next
         }
         prev!!.next = null
-
         // step 2: sort each half
         val l1 = sortList(head)
         val l2 = sortList(slow)
-
         // step 3: merge the two halves
         return merge(l1, l2)
     }
