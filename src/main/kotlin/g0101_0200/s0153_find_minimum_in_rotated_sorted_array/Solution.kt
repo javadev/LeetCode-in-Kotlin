7a56822727268573c14d60ef577c11c658db40e3
Valentyn Kolesnikov <javadev75@gmail.com>
2022-09-06 20:09:04 +0300

    Update Solution.kt

--- src/main/kotlin/g0101_0200/s0153_find_minimum_in_rotated_sorted_array/Solution.kt
+++ src/main/kotlin/g0101_0200/s0153_find_minimum_in_rotated_sorted_array/Solution.kt
@@ -2,26 +2,31 @@ package g0101_0200.s0153_find_minimum_in_rotated_sorted_array
 
 // #Medium #Top_100_Liked_Questions #Array #Binary_Search #Algorithm_II_Day_2_Binary_Search
 // #Binary_Search_I_Day_12 #Udemy_Binary_Search
-// #2022_09_06_Time_287_ms_(40.24%)_Space_35.5_MB_(84.46%)
+// #2022_09_06_Time_262_ms_(60.96%)_Space_35.4_MB_(86.45%)
 
 class Solution {
-    fun findMin(nums: IntArray): Int {
-        var left = 0
-        var right = nums.size - 1
-        while (left < right) {
-            val mid = left + (right - left) / 2
-            if (mid == left || mid == right) {
-                // this is to avoid infinite loop
-                break
-            }
-            if (nums[mid] > nums[left] && nums[mid] > nums[right]) {
-                left = mid + 1
-            } else if (nums[mid] < nums[right] && nums[mid] > nums[left]) {
-                right = mid - 1
-            } else if (nums[mid] < nums[left] && nums[mid] < nums[right]) {
-                right = mid
-            }
+    private fun findMinUtil(nums: IntArray, l: Int, r: Int): Int {
+        if (l == r) {
+            return nums[l]
         }
-        return if (right >= 0) if (nums[left] < nums[right]) nums[left] else nums[right] else nums[left]
+        val mid = (l + r) / 2
+        if (mid == l && nums[mid] < nums[r]) {
+            return nums[l]
+        }
+        if (mid - 1 >= 0 && nums[mid - 1] > nums[mid]) {
+            return nums[mid]
+        }
+        if (nums[mid] < nums[l]) {
+            return findMinUtil(nums, l, mid - 1)
+        } else if (nums[mid] > nums[r]) {
+            return findMinUtil(nums, mid + 1, r)
+        }
+        return findMinUtil(nums, l, mid - 1)
+    }
+
+    fun findMin(nums: IntArray): Int {
+        val l = 0
+        val r = nums.size - 1
+        return findMinUtil(nums, l, r)
     }
 }
