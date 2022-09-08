f255e9475a8a322672f6aa8cd5141450c0bf398a
Valentyn Kolesnikov <javadev75@gmail.com>
2022-09-08 10:44:04 +0300

    Update SolutionTest.kt

--- src/test/kotlin/g0101_0200/s0198_house_robber/SolutionTest.kt
+++ src/test/kotlin/g0101_0200/s0198_house_robber/SolutionTest.kt
@@ -14,4 +14,19 @@ internal class SolutionTest {
     fun rob2() {
         assertThat(Solution().rob(intArrayOf(2, 7, 9, 3, 1)), equalTo(12))
     }
+
+    @Test
+    fun rob3() {
+        assertThat(Solution().rob(intArrayOf()), equalTo(0))
+    }
+
+    @Test
+    fun rob4() {
+        assertThat(Solution().rob(intArrayOf(1)), equalTo(1))
+    }
+
+    @Test
+    fun rob5() {
+        assertThat(Solution().rob(intArrayOf(1, 2)), equalTo(2))
+    }
 }
