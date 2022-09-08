9c299d1cbd81356a27d7e5eed48f208e7452e113
Valentyn Kolesnikov <javadev75@gmail.com>
2022-09-08 09:54:57 +0300

    Update Solution.kt

--- src/main/kotlin/g0101_0200/s0189_rotate_array/Solution.kt
+++ src/main/kotlin/g0101_0200/s0189_rotate_array/Solution.kt
@@ -2,22 +2,26 @@ package g0101_0200.s0189_rotate_array
 
 // #Medium #Top_100_Liked_Questions #Top_Interview_Questions #Array #Math #Two_Pointers
 // #Algorithm_I_Day_2_Two_Pointers #Udemy_Arrays
-// #2022_09_07_Time_691_ms_(44.85%)_Space_73_MB_(27.44%)
+// #2022_09_08_Time_483_ms_(86.95%)_Space_71.9_MB_(50.13%)
 
 class Solution {
-    /**
-     * using an extra array of the same size to copy it
-     * O(n) time
-     * O(n) space
-     */
+    private fun reverse(nums: IntArray, l: Int, r: Int) {
+        var l = l
+        var r = r
+        while (l <= r) {
+            val temp = nums[l]
+            nums[l] = nums[r]
+            nums[r] = temp
+            l++
+            r--
+        }
+    }
+
     fun rotate(nums: IntArray, k: Int) {
-        val len = nums.size
-        val tmp = IntArray(len)
-        for (i in 0 until len) {
-            tmp[(i + k) % len] = nums[i]
-        }
-        for (i in 0 until len) {
-            nums[i] = tmp[i]
-        }
+        val n = nums.size
+        val t = n - k % n
+        reverse(nums, 0, t - 1)
+        reverse(nums, t, n - 1)
+        reverse(nums, 0, n - 1)
     }
 }
