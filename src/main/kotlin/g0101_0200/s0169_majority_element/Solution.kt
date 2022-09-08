c02051799a162f351a0b9dc3cd9d4b31646c8df4
Valentyn Kolesnikov <javadev75@gmail.com>
2022-09-08 09:53:16 +0300

    Update Solution.kt

--- src/main/kotlin/g0101_0200/s0169_majority_element/Solution.kt
+++ src/main/kotlin/g0101_0200/s0169_majority_element/Solution.kt
@@ -2,30 +2,35 @@ package g0101_0200.s0169_majority_element
 
 // #Easy #Top_100_Liked_Questions #Top_Interview_Questions #Array #Hash_Table #Sorting #Counting
 // #Divide_and_Conquer #Data_Structure_II_Day_1_Array #Udemy_Famous_Algorithm
-// #2022_09_07_Time_242_ms_(97.80%)_Space_42.4_MB_(95.06%)
+// #2022_09_08_Time_460_ms_(51.25%)_Space_52.9_MB_(52.64%)
 
 class Solution {
-    /**
-     * Moore Voting Algorithm
-     * How to understand this:
-     * 1. For a number to qualify as a majority element, it needs to occur more than 1/2 times, which
-     * means there are a max of only one such element in any given array.
-     * 2. E.g. given this array [1,2,3,1,1,1], two of the 1s will be balanced off by 2 and 3, still there are two 1s remaining in the end
-     * which is the majority element
-     */
-    fun majorityElement(nums: IntArray): Int {
+    fun majorityElement(arr: IntArray): Int {
         var count = 1
-        var majority = nums[0]
-        for (i in 1 until nums.size) {
-            if (count == 0) {
-                count++
-                majority = nums[i]
-            } else if (nums[i] == majority) {
+        var majority = arr[0]
+        // For Potential Majority Element
+        for (i in 1 until arr.size) {
+            if (arr[i] == majority) {
                 count++
             } else {
-                count--
+                if (count > 1) {
+                    count--
+                } else {
+                    majority = arr[i]
+                }
             }
         }
-        return majority
+        // For Confirmation
+        count = 0
+        for (j in arr) {
+            if (j == majority) {
+                count++
+            }
+        }
+        return if (count >= arr.size / 2 + 1) {
+            majority
+        } else {
+            -1
+        }
     }
 }
