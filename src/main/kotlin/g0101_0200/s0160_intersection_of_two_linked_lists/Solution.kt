6e08d87c0ad732f4d10c49c124728b0bb7dd4db1
Valentyn Kolesnikov <javadev75@gmail.com>
2022-09-08 09:50:34 +0300

    Update Solution.kt

--- src/main/kotlin/g0101_0200/s0160_intersection_of_two_linked_lists/Solution.kt
+++ src/main/kotlin/g0101_0200/s0160_intersection_of_two_linked_lists/Solution.kt
@@ -2,43 +2,27 @@ package g0101_0200.s0160_intersection_of_two_linked_lists
 
 // #Easy #Top_100_Liked_Questions #Top_Interview_Questions #Hash_Table #Two_Pointers #Linked_List
 // #Data_Structure_II_Day_11_Linked_List #Udemy_Linked_List
-// #2022_09_07_Time_169_ms_(100.00%)_Space_39_MB_(89.58%)
+// #2022_09_08_Time_262_ms_(83.50%)_Space_54.7_MB_(75.26%)
 
 import com_github_leetcode.ListNode
 
+/*
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
 class Solution {
-    /**
-     * Time: O(max(m, n))
-     * Space: O(1)
-     */
     fun getIntersectionNode(headA: ListNode?, headB: ListNode?): ListNode? {
-        var headA = headA
-        var headB = headB
-        var lenA = findLen(headA)
-        var lenB = findLen(headB)
-        /**align headA and headB to the same starting point and then move together until we find the intersection point */
-        while (lenA < lenB) {
-            headB = headB!!.next
-            lenB--
+        var node1 = headA
+        var node2 = headB
+        while (node1 !== node2) {
+            node1 = if (node1 == null) headB else node1.next
+            node2 = if (node2 == null) headA else node2.next
         }
-        while (lenB < lenA) {
-            headA = headA!!.next
-            lenA--
-        }
-        while (headA != headB) {
-            headA = headA!!.next
-            headB = headB!!.next
-        }
-        return headA
-    }
-
-    private fun findLen(head: ListNode?): Int {
-        var head = head
-        var len = 0
-        while (head != null) {
-            head = head.next
-            len++
-        }
-        return len
+        return node1
     }
 }
