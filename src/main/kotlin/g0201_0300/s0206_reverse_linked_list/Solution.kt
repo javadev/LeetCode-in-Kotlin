3875f38d7850201ef27322d3b5112a30ddbe4359
Valentyn Kolesnikov <javadev75@gmail.com>
2022-09-09 04:22:45 +0300

    Update Solution.kt

--- src/main/kotlin/g0201_0300/s0206_reverse_linked_list/Solution.kt
+++ src/main/kotlin/g0201_0300/s0206_reverse_linked_list/Solution.kt
@@ -3,15 +3,24 @@ package g0201_0300.s0206_reverse_linked_list
 // #Easy #Top_100_Liked_Questions #Top_Interview_Questions #Linked_List #Recursion
 // #Data_Structure_I_Day_8_Linked_List #Algorithm_I_Day_10_Recursion_Backtracking
 // #Level_1_Day_3_Linked_List #Udemy_Linked_List
-// #2022_09_08_Time_204_ms_(82.25%)_Space_36.8_MB_(21.30%)
+// #2022_09_09_Time_309_ms_(15.90%)_Space_36.7_MB_(23.71%)
 
 import com_github_leetcode.ListNode
 
+/*
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
 class Solution {
     fun reverseList(head: ListNode?): ListNode? {
         var prev: ListNode? = null
         var curr = head
-        while (curr != null) {
+        while (curr !== null) {
             val next = curr.next
             curr.next = prev
             prev = curr
