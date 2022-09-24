bbe5af0e12112b582283f4db8511908535fc9779
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2022-09-24 22:41:46 +0700

    Added tasks 69, 71, 77, 80.

---
+++ src/test/kotlin/g0001_0100/s0077_combinations/SolutionTest.kt
@@ -0,0 +1,23 @@
+package g0001_0100.s0077_combinations
+
+import com_github_leetcode.ArrayUtils.getLists
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
+
+internal class SolutionTest {
+    @Test
+    fun combine() {
+        val expected = arrayOf(
+            intArrayOf(1, 2), intArrayOf(1, 3), intArrayOf(1, 4),
+            intArrayOf(2, 3), intArrayOf(2, 4), intArrayOf(3, 4)
+        )
+        assertThat(Solution().combine(4, 2), equalTo(getLists(expected)))
+    }
+
+    @Test
+    fun combine2() {
+        val expected = arrayOf(intArrayOf(1))
+        assertThat(Solution().combine(1, 1), equalTo(getLists(expected)))
+    }
+}
