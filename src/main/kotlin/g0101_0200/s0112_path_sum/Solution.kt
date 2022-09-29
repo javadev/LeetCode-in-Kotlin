ad937b03e57ee8abb320fd42c6423cf38b42b710
Valentyn Kolesnikov <javadev75@gmail.com>
2022-09-29 18:23:31 +0300

    Update Solution.kt

--- src/main/kotlin/g0101_0200/s0112_path_sum/Solution.kt
+++ src/main/kotlin/g0101_0200/s0112_path_sum/Solution.kt
@@ -5,7 +5,7 @@ import com_github_leetcode.TreeNode
 // #Easy #Depth_First_Search #Breadth_First_Search #Tree #Binary_Tree #Data_Structure_I_Day_12_Tree
 // #2022_09_29_Time_322_ms_(36.41%)_Space_39.6_MB_(18.97%)
 
-/**
+/*
  * Example:
  * var ti = TreeNode(5)
  * var v = ti.`val`
