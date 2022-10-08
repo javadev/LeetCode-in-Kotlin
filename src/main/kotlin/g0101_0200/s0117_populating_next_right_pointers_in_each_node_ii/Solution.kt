7320ecc549f4819f4856ccec8e675fca65547206
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-10-08 12:49:08 +0700

    Update Solution.kt

--- src/main/kotlin/g0101_0200/s0117_populating_next_right_pointers_in_each_node_ii/Solution.kt
+++ src/main/kotlin/g0101_0200/s0117_populating_next_right_pointers_in_each_node_ii/Solution.kt
@@ -2,9 +2,11 @@ package g0101_0200.s0117_populating_next_right_pointers_in_each_node_ii
 
 // #Medium #Depth_First_Search #Breadth_First_Search #Tree #Binary_Tree #Linked_List
 // #Algorithm_II_Day_7_Breadth_First_Search_Depth_First_Search
-// #2022_10_08_Time_409_ms_(13.33%)_Space_43.4_MB_(13.33%)
+// #2022_10_08_Time_199_ms_(94.67%)_Space_37_MB_(93.33%)
 
 import com_github_leetcode.left_right.Node
+import java.util.LinkedList
+import java.util.Queue
 
 /*
  * Definition for a Node.
@@ -16,41 +18,32 @@ import com_github_leetcode.left_right.Node
  */
 class Solution {
     fun connect(root: Node?): Node? {
-        if (root == null) {
-            return null
-        }
-        if (root.left == null && root.right == null) {
-            return root
-        }
-        if (root.left != null) {
-            if (root.right != null) {
-                root.left!!.next = root.right
-            } else if (root.next != null) {
-                root.left!!.next = adjacentRightNode(root.next)
-            }
-        }
-        if (root.right != null) {
-            root.right!!.next = adjacentRightNode(root.next)
-        }
-        connect(root.right)
-        connect(root.left)
-        return root
-    }
 
-    private fun adjacentRightNode(root: Node?): Node? {
-        var temp = root
-        while (temp != null) {
-            if (temp.left == null && temp.right == null) {
-                temp = temp.next
-            } else {
-                if (temp.left != null) {
-                    return temp.left
-                }
-                if (temp.right != null) {
-                    return temp.right
-                }
+        if (root == null) return null
+
+        val bfsQueue: Queue<Node> = LinkedList()
+
+        bfsQueue.offer(root)
+        root.next = null
+
+        var temp: Node? = null
+        var prev: Node? = null
+
+        while (!bfsQueue.isEmpty()) {
+
+            val size = bfsQueue.size
+            prev = null
+
+            for (j in 0 until size) {
+
+                temp = bfsQueue.poll()
+                if (prev != null) prev.next = temp
+                if (temp!!.left != null) bfsQueue.offer(temp.left)
+                if (temp.right != null) bfsQueue.offer(temp.right)
+                prev = temp
             }
         }
-        return null
+
+        return root
     }
 }
