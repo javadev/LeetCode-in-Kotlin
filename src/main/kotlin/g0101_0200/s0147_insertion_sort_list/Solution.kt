e75fd7d23aed92ff42d7a67c4ed5890d8ef04453
Valentyn Kolesnikov <javadev75@gmail.com>
2022-10-09 09:55:13 +0300

    Update Solution.kt

--- src/main/kotlin/g0101_0200/s0147_insertion_sort_list/Solution.kt
+++ src/main/kotlin/g0101_0200/s0147_insertion_sort_list/Solution.kt
@@ -4,7 +4,7 @@ package g0101_0200.s0147_insertion_sort_list
 
 import com_github_leetcode.ListNode
 
-/**
+/*
  * Example:
  * var li = ListNode(5)
  * var v = li.`val`
@@ -17,20 +17,16 @@ class Solution {
     fun insertionSortList(head: ListNode?): ListNode? {
         val dummy = ListNode(-1)
         var node = head
-
         while (node != null) {
             var prev = dummy
-
             while (prev?.next != null && node.`val` > prev.next!!.`val`) {
                 prev = prev.next!!
             }
-
             val temp = node.next
             node.next = prev.next
             prev.next = node
             node = temp
         }
-
         return dummy.next
     }
 }
