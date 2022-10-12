dae3528a0a510e28c77b3c621f24c58efa8a294c
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-10-12 23:00:06 +0700

    Update Solution.kt

--- src/main/kotlin/g0101_0200/s0171_excel_sheet_column_number/Solution.kt
+++ src/main/kotlin/g0101_0200/s0171_excel_sheet_column_number/Solution.kt
@@ -8,7 +8,7 @@ class Solution {
         var num = 0
         var pow = 0
         for (i in s.length - 1 downTo 0) {
-            num += Math.pow(26.0, pow++.toDouble()).toInt() * (s[i].toInt() - 'A'.toInt() + 1)
+            num += Math.pow(26.0, pow++.toDouble()).toInt() * (s[i].code - 'A'.code + 1)
         }
         return num
     }
