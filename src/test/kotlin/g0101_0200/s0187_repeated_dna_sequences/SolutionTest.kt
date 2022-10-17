e5d0ed61966acb3599f3f256976fba5e111c3a52
Valentyn Kolesnikov <javadev75@gmail.com>
2022-10-17 19:51:00 +0300

    Update SolutionTest.kt

--- src/test/kotlin/g0101_0200/s0187_repeated_dna_sequences/SolutionTest.kt
+++ src/test/kotlin/g0101_0200/s0187_repeated_dna_sequences/SolutionTest.kt
@@ -3,14 +3,13 @@ package g0101_0200.s0187_repeated_dna_sequences
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
-import java.util.Arrays
 
 internal class SolutionTest {
     @Test
     fun findRepeatedDnaSequences() {
         assertThat(
             Solution().findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"),
-            equalTo(Arrays.asList("AAAAACCCCC", "CCCCCAAAAA"))
+            equalTo(listOf("AAAAACCCCC", "CCCCCAAAAA"))
         )
     }
 
