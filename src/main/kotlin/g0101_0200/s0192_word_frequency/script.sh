9e4d7896e56c14e3b3f6e201970d1c0ab5df9384
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-10-18 22:42:44 +0700

    update solution

--- src/main/kotlin/g0101_0200/s0192_word_frequency/script.sh
+++ src/main/kotlin/g0101_0200/s0192_word_frequency/script.sh
@@ -1,3 +1,3 @@
 # Read from the file words.txt and output the word frequency list to stdout.
-# #Medium #Shell #2022_06_28_Time_143_ms_(54.55%)_Space_3.7_MB_(85.88%)
-sed -e 's/ /\n/g' words.txt | sed -e '/^$/d' | sort | uniq -c | sort -r | awk '{print $2" "$1}'
+# #Medium #Shell #2022_10_18_Time_114_ms_(73.60%)_Space_3.8_MB_(32.09%)sed -e 's/ /\n/g' words.txt |
+sed -e '/^$/d' | sort | uniq -c | sort -r | awk '{print $2" "$1}'
