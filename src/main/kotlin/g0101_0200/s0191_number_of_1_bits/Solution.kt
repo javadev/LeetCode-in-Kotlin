dab51d2d0ce731a1e6d0243b9d564749b47f597f
Valentyn Kolesnikov <javadev75@gmail.com>
2022-10-19 04:17:08 +0300

    Update Solution.kt

--- src/main/kotlin/g0101_0200/s0191_number_of_1_bits/Solution.kt
+++ src/main/kotlin/g0101_0200/s0191_number_of_1_bits/Solution.kt
@@ -5,9 +5,8 @@ package g0101_0200.s0191_number_of_1_bits
 // #2022_10_18_Time_237_ms_(68.44%)_Space_33.8_MB_(52.46%)
 
 class Solution {
-    fun hammingWeight(n:Int):Int {
+    fun hammingWeight(n: Int): Int {
         val str = Integer.toBinaryString(n).filter { it == '1' }
-
         return str.length
     }
 }
