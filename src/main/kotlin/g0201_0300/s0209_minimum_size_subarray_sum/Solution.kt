30cdeb9b4461eb2c25b485deeb4f2c03ce666346
Valentyn Kolesnikov <javadev75@gmail.com>
2022-10-20 19:06:33 +0300

    Update Solution.kt

--- src/main/kotlin/g0201_0300/s0209_minimum_size_subarray_sum/Solution.kt
+++ src/main/kotlin/g0201_0300/s0209_minimum_size_subarray_sum/Solution.kt
@@ -5,18 +5,15 @@ package g0201_0300.s0209_minimum_size_subarray_sum
 
 class Solution {
     fun minSubArrayLen(s: Int, nums: IntArray): Int {
-
         var sum = 0
         var start = 0
         var minLength = Integer.MAX_VALUE
         var end = 0
-
-        if (nums.size < 1)
+        if (nums.size < 1) {
             return 0
-
+        }
         while (end < nums.size) {
             sum += nums[end]
-
             while (sum >= s) {
                 minLength = Math.min(minLength, end - start + 1)
                 sum -= nums[start++]
