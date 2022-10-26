f3f0f3530444fe5720904bc7eeae567446eb6a7c
Valentyn Kolesnikov <javadev75@gmail.com>
2022-10-26 22:22:18 +0300

    Update Solution.kt

--- src/main/kotlin/g0201_0300/s0235_lowest_common_ancestor_of_a_binary_search_tree/Solution.kt
+++ src/main/kotlin/g0201_0300/s0235_lowest_common_ancestor_of_a_binary_search_tree/Solution.kt
@@ -12,21 +12,17 @@ import com_github_leetcode.TreeNode
  *     var right: TreeNode? = null
  * }
  */
-
 class Solution {
     fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
         if (root == null) {
             return null
         }
-
         if (p!!.`val` < root.`val` && q!!.`val` < root.`val`) {
             return lowestCommonAncestor(root.left, q, p)
         }
-
         if (p.`val` > root.`val` && q!!.`val` > root.`val`) {
             return lowestCommonAncestor(root.right, q, p)
         }
-
         return root
     }
 }
