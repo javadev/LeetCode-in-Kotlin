f82ab33030af3cfcdc73a1fb45f945285b99518a
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2022-11-01 23:50:52 +0700

    Added tasks 257, 258, 260, 262.

---
+++ src/test/kotlin/g0201_0300/s0260_single_number_iii/SolutionTest.kt
@@ -0,0 +1,20 @@
+package g0201_0300.s0260_single_number_iii
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
+
+internal class SolutionTest {
+    @Test
+    fun singleNumber() {
+        assertThat(
+            Solution().singleNumber(intArrayOf(1, 2, 1, 3, 2, 5)),
+            equalTo(intArrayOf(3, 5))
+        )
+    }
+
+    @Test
+    fun singleNumber2() {
+        assertThat(Solution().singleNumber(intArrayOf(-1, 0)), equalTo(intArrayOf(-1, 0)))
+    }
+}
