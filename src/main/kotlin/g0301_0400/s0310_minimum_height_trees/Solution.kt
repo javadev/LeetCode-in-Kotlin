6257b28b2c43ef36b062c928c19b3a0f8fd4e871
Valentyn Kolesnikov <javadev75@gmail.com>
2022-11-09 17:07:38 +0200

    Update Solution.kt

--- src/main/kotlin/g0301_0400/s0310_minimum_height_trees/Solution.kt
+++ src/main/kotlin/g0301_0400/s0310_minimum_height_trees/Solution.kt
@@ -6,52 +6,41 @@ package g0301_0400.s0310_minimum_height_trees
 class Solution {
     fun findMinHeightTrees(n: Int, edges: Array<IntArray>): List<Int> {
         if (n == 1) return mutableListOf(0)
-
         val degrees = IntArray(n) { 0 }
         val graph = buildGraph(degrees, n, edges)
-
         val queue = ArrayDeque<Int>()
         for ((idxNode, degree) in degrees.withIndex()) {
             if (degree == 1) {
                 queue.addLast(idxNode)
             }
         }
-
         var ans = mutableListOf<Int>()
         while (queue.isNotEmpty()) {
             val size = queue.size
             val newLeaves = mutableListOf<Int>()
-
             for (_sz in 0 until size) {
                 val cur = queue.removeFirst()
                 newLeaves.add(cur)
-
                 for (next in graph[cur]) {
                     if (--degrees[next] == 1) {
                         queue.addLast(next)
                     }
                 }
             }
-
             ans = newLeaves
         }
-
         return ans
     }
 
     private fun buildGraph(degrees: IntArray, n: Int, edges: Array<IntArray>): Array<ArrayList<Int>> {
         val graph = Array(n) { arrayListOf<Int>() }
-
         for (edge in edges) {
             val (u, v) = edge
-
             graph[u].add(v)
             graph[v].add(u)
-
             ++degrees[u]
             ++degrees[v]
         }
-
         return graph
     }
 }
