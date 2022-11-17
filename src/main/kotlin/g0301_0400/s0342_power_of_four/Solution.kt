27056fb14983e83518e18dae96f47935c81ebc1b
Valentyn Kolesnikov <javadev75@gmail.com>
2022-11-17 18:47:29 +0200

    Update Solution.kt

--- src/main/kotlin/g0301_0400/s0342_power_of_four/Solution.kt
+++ src/main/kotlin/g0301_0400/s0342_power_of_four/Solution.kt
@@ -2,6 +2,7 @@ package g0301_0400.s0342_power_of_four
 
 // #Easy #Math #Bit_Manipulation #Recursion #2022_11_17_Time_150_ms_(92.11%)_Space_33.4_MB_(89.47%)
 
+@Suppress("NAME_SHADOWING")
 class Solution {
     fun isPowerOfFour(n: Int): Boolean {
         var n = n
