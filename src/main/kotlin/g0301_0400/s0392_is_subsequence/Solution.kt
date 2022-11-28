68866be53d2adf5ef58a23c57ecde43e598c1468
Valentyn Kolesnikov <javadev75@gmail.com>
2022-11-28 17:07:48 +0200

    Create Solution.kt

--- src/main/kotlin/g0301_0400/s0392_is_subsequence/Solution.kt
+++ src/main/kotlin/g0301_0400/s0392_is_subsequence/Solution.kt
@@ -8,12 +8,10 @@ class Solution {
     fun isSubsequence(s: String, t: String): Boolean {
         var i = 0
         var j = 0
-
         while (i < s.length && j < t.length) {
             if (s[i] == t[j]) i++
             j++
         }
-
         return i == s.length
     }
 }
