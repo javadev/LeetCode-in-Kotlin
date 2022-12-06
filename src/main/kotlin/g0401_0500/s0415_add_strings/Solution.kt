5d06808eb40e1b85cbbd9f835f5a79d8a5048f5b
Valentyn Kolesnikov <javadev75@gmail.com>
2022-12-06 19:03:00 +0200

    Update Solution.kt

--- src/main/kotlin/g0401_0500/s0415_add_strings/Solution.kt
+++ src/main/kotlin/g0401_0500/s0415_add_strings/Solution.kt
@@ -10,25 +10,18 @@ class Solution {
         val res = StringBuilder()
         var carry = 0
         var sum: Int
-
         while (endNum1 >= 0 || endNum2 >= 0) {
-
             val a = if (endNum1 >= 0) num1[endNum1] - '0' else 0
             val b = if (endNum2 >= 0) num2[endNum2] - '0' else 0
-
             sum = (a + b + carry) % 10
             carry = (a + b + carry) / 10
-
             res.append(sum)
-
             endNum1--
             endNum2--
         }
-
         if (carry != 0) {
             res.append(carry)
         }
-
         return res.reverse().toString()
     }
 }
