457081db0274c3c1f5450719d32bc28b741c5a0b
Valentyn Kolesnikov <javadev75@gmail.com>
2022-12-23 20:26:02 +0200

    Update Solution.kt

--- src/main/kotlin/g0401_0500/s0434_number_of_segments_in_a_string/Solution.kt
+++ src/main/kotlin/g0401_0500/s0434_number_of_segments_in_a_string/Solution.kt
@@ -2,6 +2,7 @@ package g0401_0500.s0434_number_of_segments_in_a_string
 
 // #Easy #String #2022_12_22_Time_167_ms_(80.00%)_Space_35.4_MB_(46.67%)
 
+@Suppress("NAME_SHADOWING")
 class Solution {
     fun countSegments(s: String): Int {
         var s = s
