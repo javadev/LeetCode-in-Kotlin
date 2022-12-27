0a029eb6db2daaa23f34a4258be208b94ca3554b
Valentyn Kolesnikov <javadev75@gmail.com>
2022-12-28 01:21:45 +0200

    Update Solution.kt

--- src/main/kotlin/g0401_0500/s0458_poor_pigs/Solution.kt
+++ src/main/kotlin/g0401_0500/s0458_poor_pigs/Solution.kt
@@ -3,6 +3,7 @@ package g0401_0500.s0458_poor_pigs
 // #Hard #Dynamic_Programming #Math #Combinatorics
 // #2022_12_27_Time_133_ms_(80.00%)_Space_32.8_MB_(80.00%)
 
+@Suppress("NAME_SHADOWING")
 class Solution {
     fun poorPigs(buckets: Int, minutesToDie: Int, minutesToTest: Int): Int {
         var buckets = buckets
