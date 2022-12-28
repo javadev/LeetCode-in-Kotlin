992bbd687b40834560fddc475a4815e5499a8a93
Valentyn Kolesnikov <javadev75@gmail.com>
2022-12-28 19:33:50 +0200

    Update Solution.kt

--- src/main/kotlin/g0401_0500/s0462_minimum_moves_to_equal_array_elements_ii/Solution.kt
+++ src/main/kotlin/g0401_0500/s0462_minimum_moves_to_equal_array_elements_ii/Solution.kt
@@ -2,11 +2,9 @@ package g0401_0500.s0462_minimum_moves_to_equal_array_elements_ii
 
 // #Medium #Array #Math #Sorting #2022_12_28_Time_210_ms_(100.00%)_Space_38.1_MB_(87.50%)
 
-import java.util.Arrays
-
 class Solution {
     fun minMoves2(nums: IntArray): Int {
-        Arrays.sort(nums)
+        nums.sort()
         val median = (nums.size - 1) / 2
         var ops = 0
         for (num in nums) {
