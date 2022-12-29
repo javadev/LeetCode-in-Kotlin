ac35ac0b536f6f2f4b177b60b9bf3d4e5921e500
Valentyn Kolesnikov <javadev75@gmail.com>
2022-12-29 09:32:50 +0200

    Update SolutionTest.kt

--- src/test/kotlin/g0401_0500/s0448_find_all_numbers_disappeared_in_an_array/SolutionTest.kt
+++ src/test/kotlin/g0401_0500/s0448_find_all_numbers_disappeared_in_an_array/SolutionTest.kt
@@ -3,21 +3,20 @@ package g0401_0500.s0448_find_all_numbers_disappeared_in_an_array
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
-import java.util.Arrays
 
 internal class SolutionTest {
     @Test
     fun findDisappearedNumbers() {
         assertThat(
             Solution().findDisappearedNumbers(intArrayOf(4, 3, 2, 7, 8, 2, 3, 1)),
-            equalTo(Arrays.asList(5, 6))
+            equalTo(listOf(5, 6))
         )
     }
 
     @Test
     fun findDisappearedNumbers2() {
         assertThat(
-            Solution().findDisappearedNumbers(intArrayOf(1, 1)), equalTo(Arrays.asList(2))
+            Solution().findDisappearedNumbers(intArrayOf(1, 1)), equalTo(listOf(2))
         )
     }
 }
