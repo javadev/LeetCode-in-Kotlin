15b8659a7b03baaec649adb09a6661e5c19b6cae
Valentyn Kolesnikov <javadev75@gmail.com>
2022-12-29 15:36:56 +0200

    Update Solution.kt

--- src/main/kotlin/g0401_0500/s0467_unique_substrings_in_wraparound_string/Solution.kt
+++ src/main/kotlin/g0401_0500/s0467_unique_substrings_in_wraparound_string/Solution.kt
@@ -9,14 +9,14 @@ class Solution {
         val map = IntArray(26)
         var len = 0
         for (i in 0 until n) {
-            if (i > 0 && (str[i - 1].toInt() + 1 == str[i].toInt() || str[i - 1] == 'z' && str[i] == 'a')) {
+            if (i > 0 && (str[i - 1].code + 1 == str[i].code || str[i - 1] == 'z' && str[i] == 'a')) {
                 len += 1
             } else {
                 len = 1
             }
             // we are storing the max len of string for each letter and then we will count all these
             // length.
-            map[str[i].toInt() - 'a'.toInt()] = Math.max(map[str[i].toInt() - 'a'.toInt()], len)
+            map[str[i].code - 'a'.code] = Math.max(map[str[i].code - 'a'.code], len)
         }
         var answer = 0
         for (num in map) {
