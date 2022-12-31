3c92dbf5e3ef83965c8d47b6f0f8b233a103c64b
Valentyn Kolesnikov <javadev75@gmail.com>
2022-12-31 16:01:08 +0200

    Update Solution.kt

--- src/main/kotlin/g0401_0500/s0475_heaters/Solution.kt
+++ src/main/kotlin/g0401_0500/s0475_heaters/Solution.kt
@@ -3,8 +3,6 @@ package g0401_0500.s0475_heaters
 // #Medium #Array #Sorting #Binary_Search #Two_Pointers
 // #2022_12_31_Time_356_ms_(87.50%)_Space_44.6_MB_(87.50%)
 
-import java.util.Arrays
-
 class Solution {
     fun findRadius(houses: IntArray, heaters: IntArray): Int {
         var res = 0
@@ -12,8 +10,8 @@ class Solution {
         val n = heaters.size
         var hs = 0
         var ht = 0
-        Arrays.sort(houses)
-        Arrays.sort(heaters)
+        houses.sort()
+        heaters.sort()
         if (n == 1) {
             return Math.max(Math.abs(houses[0] - heaters[0]), Math.abs(houses[m - 1] - heaters[0]))
         }
