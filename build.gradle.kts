857ec9c32087d51b444a0cd0beff8cc8b1947d5c
Valentyn Kolesnikov <javadev75@gmail.com>
2023-01-04 00:48:59 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ description = "leetcode-in-kotlin"
 java.sourceCompatibility = JavaVersion.VERSION_11
 java.targetCompatibility = JavaVersion.VERSION_11
 
-tasks.withType<KotlinCompile> {
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
 }
 
