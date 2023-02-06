9d4f38e5d4c976d095f1b0c12ec77e662226b2d1
ThanhNIT <thanhnguyen.it.work@gmail.com>
2023-02-06 20:30:37 +0700

    Update script.sql

--- src/main/kotlin/g0601_0700/s0620_not_boring_movies/script.sql
+++ src/main/kotlin/g0601_0700/s0620_not_boring_movies/script.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 # #Easy #Database #2023_02_06_Time_305_ms_(59.80%)_Space_0B_(100.00%)
-select * from Cinema
+select id, movie, description, rating from Cinema
 WHERE mod(id,2) = 1
 and
 description not LIKE '%boring%'
