6e878a749a419bdc9904e962be7735f7b43d8fd6
ThanhNIT <thanhnguyen.it.work@gmail.com>
2023-02-13 20:59:40 +0700

    Update Solution.kt

--- src/main/kotlin/g0601_0700/s0657_robot_return_to_origin/Solution.kt
+++ src/main/kotlin/g0601_0700/s0657_robot_return_to_origin/Solution.kt
@@ -6,9 +6,9 @@ class Solution {
     fun judgeCircle(moves: String): Boolean {
         val map = IntArray(26)
         for (c in moves.toCharArray()) {
-            map[c.toInt() - 'A'.toInt()]++
+            map[c.code - 'A'.code]++
         }
-        return map['U'.toInt() - 'A'.toInt()] == map['D'.toInt() - 'A'.toInt()] &&
-            map['L'.toInt() - 'A'.toInt()] == map['R'.toInt() - 'A'.toInt()]
+        return map['U'.code - 'A'.code] == map['D'.code - 'A'.code] &&
+            map['L'.code - 'A'.code] == map['R'.code - 'A'.code]
     }
 }
