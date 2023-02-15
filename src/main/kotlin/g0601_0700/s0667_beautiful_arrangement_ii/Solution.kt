aa96546c517b4c4953674dc9ffd02179395de0e8
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-15 15:06:26 +0200

    Update Solution.kt

--- src/main/kotlin/g0601_0700/s0667_beautiful_arrangement_ii/Solution.kt
+++ src/main/kotlin/g0601_0700/s0667_beautiful_arrangement_ii/Solution.kt
@@ -2,6 +2,7 @@ package g0601_0700.s0667_beautiful_arrangement_ii
 
 // #Medium #Array #Math #2023_02_14_Time_175_ms_(100.00%)_Space_36.1_MB_(100.00%)
 
+@Suppress("NAME_SHADOWING")
 class Solution {
     fun constructArray(n: Int, k: Int): IntArray {
         var k = k
