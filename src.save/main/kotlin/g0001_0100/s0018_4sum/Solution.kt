f0c8af95e69e4779083a58b097603e9d56445a57
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-27 05:36:21 +0200

    Improved task 18

--- src.save/main/kotlin/g0001_0100/s0018_4sum/Solution.kt
+++ src.save/main/kotlin/g0001_0100/s0018_4sum/Solution.kt
@@ -1,58 +1,60 @@
 package g0001_0100.s0018_4sum
 
-// #Medium #Array #Sorting #Two_Pointers #2022_10_06_Time_244_ms_(100.00%)_Space_38.8_MB_(100.00%)
+// #Medium #Array #Sorting #Two_Pointers #2023_02_27_Time_221_ms_(100.00%)_Space_36.8_MB_(100.00%)
 
 class Solution {
     fun fourSum(nums: IntArray, target: Int): List<List<Int>> {
-        val ret: MutableList<List<Int>> = ArrayList()
-        if (nums.size < 4) {
-            return ret
-        }
-        if (nums[0] == 1000000000 && nums[1] == 1000000000) {
-            return ret
-        }
+        val n = nums.size
         nums.sort()
-        for (i in 0 until nums.size - 3) {
-            if (i != 0 && nums[i] == nums[i - 1]) {
+        val result: MutableList<List<Int>> = ArrayList()
+        for (i in 0 until n - 3) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue
             }
-            for (j in i + 1 until nums.size - 2) {
-                if (j != i + 1 && nums[j] == nums[j - 1]) {
+            if (nums[i].toLong() + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break
+            }
+            if (nums[i].toLong() + nums[n - 3] + nums[n - 2] + nums[n - 1] < target) {
+                continue
+            }
+            for (j in i + 1 until n - 2) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue
                 }
-                var left = j + 1
-                var right = nums.size - 1
-                val half = nums[i] + nums[j]
-                if (half + nums[left] + nums[left + 1] > target) {
+                if (nums[j].toLong() + nums[j + 1] + nums[j + 2] > target - nums[i]) {
+                    break
+                }
+                if (nums[j].toLong() + nums[n - 2] + nums[n - 1] < target - nums[i]) {
                     continue
                 }
-                if (half + nums[right] + nums[right - 1] < target) {
-                    continue
-                }
-                while (left < right) {
-                    val sum = nums[left] + nums[right] + half
-                    if (sum == target) {
-                        ret.add(listOf(nums[left++], nums[right--], nums[i], nums[j]))
-                        while (nums[left] == nums[left - 1] && left < right) {
-                            left++
+                val tempTarget = target - (nums[i] + nums[j])
+                var low = j + 1
+                var high = n - 1
+                while (low < high) {
+                    val curSum = nums[low] + nums[high]
+                    if (curSum == tempTarget) {
+                        val tempList: MutableList<Int> = ArrayList()
+                        tempList.add(nums[i])
+                        tempList.add(nums[j])
+                        tempList.add(nums[low])
+                        tempList.add(nums[high])
+                        result.add(tempList)
+                        low++
+                        high--
+                        while (low < high && nums[low] == nums[low - 1]) {
+                            low++
                         }
-                        while (nums[right] == nums[right + 1] && left < right) {
-                            right--
+                        while (low < high && nums[high] == nums[high + 1]) {
+                            high--
                         }
-                    } else if (sum < target) {
-                        left++
-                        while (nums[left] == nums[left - 1] && left < right) {
-                            left++
-                        }
+                    } else if (curSum < tempTarget) {
+                        low++
                     } else {
-                        right--
-                        while (nums[right] == nums[right + 1] && left < right) {
-                            right--
-                        }
+                        high--
                     }
                 }
             }
         }
-        return ret
+        return result
     }
 }
