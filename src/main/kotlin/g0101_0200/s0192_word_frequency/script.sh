1d0cc6743b57e9b6e3483086c14e8ad77d2bfb88
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-02 08:35:24 +0200

    Improved task 192

--- src/main/kotlin/g0101_0200/s0192_word_frequency/script.sh
+++ src/main/kotlin/g0101_0200/s0192_word_frequency/script.sh
@@ -1,3 +1,3 @@
 # Read from the file words.txt and output the word frequency list to stdout.
-# #Medium #Shell #2022_10_18_Time_114_ms_(73.60%)_Space_3.8_MB_(32.09%)sed -e 's/ /\n/g' words.txt |
-sed -e '/^$/d' | sort | uniq -c | sort -r | awk '{print $2" "$1}'
+# #Medium #Shell #2023_03_02_Time_96_ms_(80.40%)_Space_3.9_MB_(41.71%)
+echo -e "$(cat words.txt)" | awk '{for(i=1;i<=NF;i++){if(arr[$i]==0){arr[$i]=1}else{arr[$i]=arr[$i]+1}}}END{for(i in arr) print i, arr[i]}' | sort -k2 -n -r
