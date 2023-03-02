f65765f3065d5e9b5923fb3fbd1ea5a4ab3df610
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-02 08:22:36 +0200

    Improved sort

--- src/main/kotlin/g0101_0200/s0179_largest_number/Solution.kt
+++ src/main/kotlin/g0101_0200/s0179_largest_number/Solution.kt
@@ -3,8 +3,6 @@ package g0101_0200.s0179_largest_number
 // #Medium #Top_Interview_Questions #String #Sorting #Greedy
 // #2023_03_02_Time_187_ms_(100.00%)_Space_36.7_MB_(76.56%)
 
-import java.util.Arrays
-
 class Solution {
     fun largestNumber(nums: IntArray): String {
         val n = nums.size
@@ -12,7 +10,7 @@ class Solution {
         for (i in 0 until n) {
             s[i] = nums[i].toString()
         }
-        Arrays.sort(s) { a, b -> (b + a).compareTo(a + b) }
+        s.sortWith { a, b -> (b + a).compareTo(a + b) }
         val sb = StringBuilder()
         for (str in s) {
             sb.append(str)
