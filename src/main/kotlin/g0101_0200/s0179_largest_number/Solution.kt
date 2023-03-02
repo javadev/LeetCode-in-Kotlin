1650ff8a2a18aa07098e3f21f73d428fded65007
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-02 08:26:28 +0200

    Improved task 179

--- src/main/kotlin/g0101_0200/s0179_largest_number/Solution.kt
+++ src/main/kotlin/g0101_0200/s0179_largest_number/Solution.kt
@@ -1,7 +1,7 @@
 package g0101_0200.s0179_largest_number
 
 // #Medium #Top_Interview_Questions #String #Sorting #Greedy
-// #2022_10_13_Time_380_ms_(43.40%)_Space_39.9_MB_(60.38%)
+// #2023_03_02_Time_187_ms_(100.00%)_Space_36.7_MB_(76.56%)
 
 class Solution {
     fun largestNumber(nums: IntArray): String {
@@ -10,11 +10,7 @@ class Solution {
         for (i in 0 until n) {
             s[i] = nums[i].toString()
         }
-        s.sortWith { a: String?, b: String? ->
-            (b + a).compareTo(
-                a + b
-            )
-        }
+        s.sortWith { a, b -> (b + a).compareTo(a + b) }
         val sb = StringBuilder()
         for (str in s) {
             sb.append(str)
