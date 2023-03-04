becef6ccf3e9407fea55038a4d1f756071edb34f
ThanhNIT <thanhnguyen.it.work@gmail.com>
2023-03-04 10:54:03 +0700

    Update WordFilter.kt

--- src/main/kotlin/g0701_0800/s0745_prefix_and_suffix_search/WordFilter.kt
+++ src/main/kotlin/g0701_0800/s0745_prefix_and_suffix_search/WordFilter.kt
@@ -47,7 +47,7 @@ class WordFilter(words: Array<String>) {
     }
 }
 
-/**
+/*
  * Your WordFilter object will be instantiated and called as such:
  * var obj = WordFilter(words)
  * var param_1 = obj.f(pref,suff)
