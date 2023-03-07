8d393a1a437c4b3402ac30f70440c49d10ef0dd0
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-07 09:55:07 +0200

    Improved task 442

--- src.save/main/kotlin/g0401_0500/s0442_find_all_duplicates_in_an_array/Solution.kt
+++ src.save/main/kotlin/g0401_0500/s0442_find_all_duplicates_in_an_array/Solution.kt
@@ -6,13 +6,11 @@ class Solution {
     fun findDuplicates(nums: IntArray): List<Int> {
         val setToCheck: MutableSet<Int> = mutableSetOf()
         val duplicates: MutableList<Int> = mutableListOf()
-
         nums.forEach { element ->
             if (!setToCheck.add(element)) {
                 duplicates.add(element)
             }
         }
-
         return duplicates
     }
 }
