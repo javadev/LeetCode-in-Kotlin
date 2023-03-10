27ba6652e6c2c167f5f33c033d649273ffe3b81c
ThanhNIT <thanhnguyen.it.work@gmail.com>
2023-03-10 20:46:07 +0700

    Update Solution.kt

--- src/main/kotlin/g0701_0800/s0771_jewels_and_stones/Solution.kt
+++ src/main/kotlin/g0701_0800/s0771_jewels_and_stones/Solution.kt
@@ -1,5 +1,7 @@
 package g0701_0800.s0771_jewels_and_stones
 
+// #Easy #String #Hash_Table #2023_03_10_Time_143_ms_(99.08%)_Space_33.5_MB_(100.00%)
+
 class Solution {
     fun numJewelsInStones(jewels: String, stones: String): Int {
         val x = IntArray(60)
