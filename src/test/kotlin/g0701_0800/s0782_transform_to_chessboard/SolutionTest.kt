e488c8be2a914c0a747a9939dabc3fca2dd80893
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-12 08:14:20 +0200

    Update SolutionTest.kt

--- src/test/kotlin/g0701_0800/s0782_transform_to_chessboard/SolutionTest.kt
+++ src/test/kotlin/g0701_0800/s0782_transform_to_chessboard/SolutionTest.kt
@@ -30,4 +30,51 @@ internal class SolutionTest {
     fun movesToChessboard3() {
         assertThat(Solution().movesToChessboard(arrayOf(intArrayOf(1, 0), intArrayOf(1, 0))), equalTo(-1))
     }
+
+    @Test
+    fun movesToChessboard4() {
+        assertThat(
+            Solution()
+                .movesToChessboard(
+                    arrayOf(
+                        intArrayOf(0, 0, 1, 1),
+                        intArrayOf(1, 1, 0, 0),
+                        intArrayOf(0, 1, 0, 1),
+                        intArrayOf(1, 0, 1, 0)
+                    )
+                ),
+            equalTo(-1)
+        )
+    }
+
+    @Test
+    fun movesToChessboard5() {
+        assertThat(
+            Solution()
+                .movesToChessboard(
+                    arrayOf(
+                        intArrayOf(1, 1, 1, 1),
+                        intArrayOf(1, 1, 1, 1),
+                        intArrayOf(0, 0, 0, 0),
+                        intArrayOf(0, 0, 0, 0)
+                    )
+                ),
+            equalTo(-1)
+        )
+    }
+
+    @Test
+    fun movesToChessboard6() {
+        assertThat(
+            Solution()
+                .movesToChessboard(
+                    arrayOf(
+                        intArrayOf(1, 1, 0),
+                        intArrayOf(0, 0, 1),
+                        intArrayOf(0, 0, 1)
+                    )
+                ),
+            equalTo(2)
+        )
+    }
 }
