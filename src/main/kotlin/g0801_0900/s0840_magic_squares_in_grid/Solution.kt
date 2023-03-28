8636dc75cbf0352f068ef649cf7fcb156e64a339
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2023-03-28 12:31:31 +0700

    Added tasks 838, 839, 840, 841

---
+++ src/main/kotlin/g0801_0900/s0840_magic_squares_in_grid/Solution.kt
@@ -0,0 +1,49 @@
+package g0801_0900.s0840_magic_squares_in_grid
+
+// #Medium #Array #Math #Matrix #2023_03_28_Time_149_ms_(100.00%)_Space_34.2_MB_(100.00%)
+
+class Solution {
+    fun numMagicSquaresInside(grid: Array<IntArray>): Int {
+        val m = grid.size
+        val n = grid[0].size
+        var count = 0
+        for (i in 0 until m - 2) {
+            for (j in 0 until n - 2) {
+                val set: MutableSet<Int> = HashSet()
+                val sum = grid[i][j] + grid[i][j + 1] + grid[i][j + 2]
+                if (sum == grid[i + 1][j] + grid[i + 1][j + 1] + grid[i + 1][j + 2] && sum == grid[i + 2][j] +
+                    grid[i + 2][j + 1] + grid[i + 2][j + 2] && sum == grid[i][j] + grid[i + 1][j] + grid[i + 2][j] &&
+                    sum == grid[i][j + 1] + grid[i + 1][j + 1] + grid[i + 2][j + 1] && sum == grid[i][j + 2] +
+                    grid[i + 1][j + 2] + grid[i + 2][j + 2] && sum == grid[i][j] + grid[i + 1][j + 1] +
+                    grid[i + 2][j + 2] && sum == grid[i][j + 2] + grid[i + 1][j + 1] + grid[i + 2][j] && set.add(
+                            grid[i][j]
+                        ) &&
+                    isLegit(grid[i][j]) &&
+                    set.add(grid[i][j + 1]) &&
+                    isLegit(grid[i][j + 1]) &&
+                    set.add(grid[i][j + 2]) &&
+                    isLegit(grid[i][j + 2]) &&
+                    set.add(grid[i + 1][j]) &&
+                    isLegit(grid[i + 1][j]) &&
+                    set.add(grid[i + 1][j + 1]) &&
+                    isLegit(grid[i + 1][j + 1]) &&
+                    set.add(grid[i + 1][j + 2]) &&
+                    isLegit(grid[i + 1][j + 2]) &&
+                    set.add(grid[i + 2][j]) &&
+                    isLegit(grid[i + 2][j]) &&
+                    set.add(grid[i + 2][j + 1]) &&
+                    isLegit(grid[i + 2][j + 1]) &&
+                    set.add(grid[i + 2][j + 2]) &&
+                    isLegit(grid[i + 2][j + 2])
+                ) {
+                    count++
+                }
+            }
+        }
+        return count
+    }
+
+    private fun isLegit(num: Int): Boolean {
+        return num in 1..9
+    }
+}
---
+++ src/test/kotlin/g0801_0900/s0840_magic_squares_in_grid/SolutionTest.kt
@@ -0,0 +1,21 @@
+package g0801_0900.s0840_magic_squares_in_grid
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
+
+internal class SolutionTest {
+    @Test
+    fun numMagicSquaresInside() {
+        assertThat(
+            Solution()
+                .numMagicSquaresInside(arrayOf(intArrayOf(4, 3, 8, 4), intArrayOf(9, 5, 1, 9), intArrayOf(2, 7, 6, 2))),
+            equalTo(1)
+        )
+    }
+
+    @Test
+    fun numMagicSquaresInside2() {
+        assertThat(Solution().numMagicSquaresInside(arrayOf(intArrayOf(8))), equalTo(0))
+    }
+}
