326c8579d4dc2535b4f58305b214377df5694fb2
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-29 12:59:14 +0300

    Update Solution.kt

--- src/main/kotlin/g0801_0900/s0846_hand_of_straights/Solution.kt
+++ src/main/kotlin/g0801_0900/s0846_hand_of_straights/Solution.kt
@@ -3,15 +3,13 @@ package g0801_0900.s0846_hand_of_straights
 // #Medium #Array #Hash_Table #Sorting #Greedy
 // #2023_03_29_Time_306_ms_(96.15%)_Space_39.4_MB_(46.15%)
 
-import java.util.Arrays
-
 class Solution {
     fun isNStraightHand(hand: IntArray, groupSize: Int): Boolean {
         val len = hand.size
         if (len % groupSize != 0) {
             return false
         }
-        Arrays.sort(hand)
+        hand.sort()
         val map: MutableMap<Int, Int> = HashMap()
         for (num in hand) {
             var cnt = map.getOrDefault(num, 0)
