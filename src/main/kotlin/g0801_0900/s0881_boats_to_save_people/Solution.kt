50d13ded493ed73977b2de1f09cd6bb9d167f542
Valentyn Kolesnikov <javadev75@gmail.com>
2023-04-09 08:27:52 +0300

    Update Solution.kt

--- src/main/kotlin/g0801_0900/s0881_boats_to_save_people/Solution.kt
+++ src/main/kotlin/g0801_0900/s0881_boats_to_save_people/Solution.kt
@@ -3,11 +3,9 @@ package g0801_0900.s0881_boats_to_save_people
 // #Medium #Array #Sorting #Greedy #Two_Pointers
 // #2023_04_08_Time_370_ms_(96.07%)_Space_44.8_MB_(99.70%)
 
-import java.util.Arrays
-
 class Solution {
     fun numRescueBoats(people: IntArray, limit: Int): Int {
-        Arrays.sort(people)
+        people.sort()
         var i = 0
         var j = people.size - 1
         var boats = 0
