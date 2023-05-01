17d849975bb3e5da77e030f4a04af18214086751
Valentyn Kolesnikov <javadev75@gmail.com>
2023-05-01 18:51:34 +0300

    Update Solution.kt

--- src/main/kotlin/g0901_1000/s0948_bag_of_tokens/Solution.kt
+++ src/main/kotlin/g0901_1000/s0948_bag_of_tokens/Solution.kt
@@ -3,13 +3,11 @@ package g0901_1000.s0948_bag_of_tokens
 // #Medium #Array #Sorting #Greedy #Two_Pointers
 // #2023_05_01_Time_161_ms_(100.00%)_Space_37.4_MB_(33.33%)
 
-import java.util.Arrays
-
 @Suppress("NAME_SHADOWING")
 class Solution {
     fun bagOfTokensScore(tokens: IntArray, power: Int): Int {
         var power = power
-        Arrays.sort(tokens)
+        tokens.sort()
         var score = 0
         var l = 0
         var r = tokens.size - 1
