70e37455dadd2a25de66bda39b54eafe58e68453
Valentyn Kolesnikov <javadev75@gmail.com>
2023-05-01 18:54:15 +0300

    Update Solution.kt

--- src/main/kotlin/g0901_1000/s0950_reveal_cards_in_increasing_order/Solution.kt
+++ src/main/kotlin/g0901_1000/s0950_reveal_cards_in_increasing_order/Solution.kt
@@ -2,11 +2,9 @@ package g0901_1000.s0950_reveal_cards_in_increasing_order
 
 // #Medium #Array #Sorting #Simulation #Queue #2023_05_01_Time_176_ms_(100.00%)_Space_39_MB_(33.33%)
 
-import java.util.Arrays
-
 class Solution {
     fun deckRevealedIncreasing(deck: IntArray): IntArray {
-        Arrays.sort(deck)
+        deck.sort()
         val n = deck.size
         val result = IntArray(n * 2)
         var idx = result.size - 1
@@ -18,6 +16,6 @@ class Solution {
             }
             result[idx--] = deck[i--]
         }
-        return Arrays.copyOfRange(result, idx + 1, lastIdx + 1)
+        return result.copyOfRange(idx + 1, lastIdx + 1)
     }
 }
