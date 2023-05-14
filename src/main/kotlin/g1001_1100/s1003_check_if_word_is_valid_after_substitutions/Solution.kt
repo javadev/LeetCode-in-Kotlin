24764d088afe5f8b69c1696bfeba50125c7bd4f1
Valentyn Kolesnikov <javadev75@gmail.com>
2023-05-14 08:38:17 +0300

    Update Solution.kt

--- src/main/kotlin/g1001_1100/s1003_check_if_word_is_valid_after_substitutions/Solution.kt
+++ src/main/kotlin/g1001_1100/s1003_check_if_word_is_valid_after_substitutions/Solution.kt
@@ -1,5 +1,7 @@
 package g1001_1100.s1003_check_if_word_is_valid_after_substitutions
 
+// #Medium #String #Stack #2023_05_14_Time_216_ms_(100.00%)_Space_46.7_MB_(50.00%)
+
 import java.util.Deque
 import java.util.LinkedList
 
