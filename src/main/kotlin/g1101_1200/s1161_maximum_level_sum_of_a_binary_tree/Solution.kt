5db34361c6b1bdf446c31d2f972ad2cb2121d0bc
Valentyn Kolesnikov <javadev75@gmail.com>
2023-05-25 10:05:35 +0300

    Update Solution.kt

--- src/main/kotlin/g1101_1200/s1161_maximum_level_sum_of_a_binary_tree/Solution.kt
+++ src/main/kotlin/g1101_1200/s1161_maximum_level_sum_of_a_binary_tree/Solution.kt
@@ -1,25 +1,21 @@
 package g1101_1200.s1161_maximum_level_sum_of_a_binary_tree
 
 // #Medium #Depth_First_Search #Breadth_First_Search #Tree #Binary_Tree
+// #2023_05_25_Time_445_ms_(87.50%)_Space_97.1_MB_(75.00%)
 
 import com_github_leetcode.TreeNode
 
 /*
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
  * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
  * }
  */
-class Solution {
+cass Solution {
     private var sums: MutableList<Int>? = null
     fun maxLevelSum(root: TreeNode?): Int {
         sums = ArrayList()
