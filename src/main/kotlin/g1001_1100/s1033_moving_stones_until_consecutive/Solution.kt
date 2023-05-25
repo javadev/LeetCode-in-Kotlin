3f449cb15dbc6d99db66bf1035dfe5f3cc1870a3
Valentyn Kolesnikov <javadev75@gmail.com>
2023-05-25 05:46:03 +0300

    Update Solution.kt

--- src/main/kotlin/g1001_1100/s1033_moving_stones_until_consecutive/Solution.kt
+++ src/main/kotlin/g1001_1100/s1033_moving_stones_until_consecutive/Solution.kt
@@ -2,8 +2,6 @@ package g1001_1100.s1033_moving_stones_until_consecutive
 
 // #Medium #Math #Brainteaser #2023_05_25_Time_139_ms_(100.00%)_Space_35.3_MB_(100.00%)
 
-import java.util.Arrays
-
 class Solution {
     private fun minMoves(x: Int, y: Int, z: Int): Int {
         if (x + 1 == y && y + 1 == z) {
@@ -20,7 +18,7 @@ class Solution {
 
     fun numMovesStones(a: Int, b: Int, c: Int): IntArray {
         val t = intArrayOf(a, b, c)
-        Arrays.sort(t)
+        t.sort()
         val min = minMoves(t[0], t[1], t[2])
         val max = maxMoves(t[0], t[2])
         return intArrayOf(min, max)
