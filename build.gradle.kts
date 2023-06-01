31b86b6989b4b2c68663a2d6f8b0b7376027932f
Valentyn Kolesnikov <javadev75@gmail.com>
2023-06-01 13:29:50 +0300

    Introduced version 1.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ tasks.test {
 }
 
 group = "com.github.javadev"
-version = "1.12-SNAPSHOT"
+version = "1.13-SNAPSHOT"
 description = "leetcode-in-kotlin"
 java.sourceCompatibility = JavaVersion.VERSION_11
 java.targetCompatibility = JavaVersion.VERSION_11
