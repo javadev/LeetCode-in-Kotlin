43384322713f917639d26e03f93b97395c21d7a5
Valentyn Kolesnikov <javadev75@gmail.com>
2023-06-05 08:20:53 +0300

    Update MountainArray.kt

--- src/main/kotlin/g1001_1100/s1095_find_in_mountain_array/MountainArray.kt
+++ src/main/kotlin/g1001_1100/s1095_find_in_mountain_array/MountainArray.kt
@@ -2,9 +2,9 @@ package g1001_1100.s1095_find_in_mountain_array
 
 open class MountainArray {
     open fun get(index: Int): Int {
-        return TODO("Provide the return value")
+        return 0
     }
     open fun length(): Int {
-        return TODO("Provide the return value")
+        return 0
     }
 }
