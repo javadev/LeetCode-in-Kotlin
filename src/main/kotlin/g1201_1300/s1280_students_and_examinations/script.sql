07dd978fcddc7d66f6188b2ecb08e816652161db
Valentyn Kolesnikov <javadev75@gmail.com>
2023-06-10 09:56:29 +0300

    Update script.sql

--- src/main/kotlin/g1201_1300/s1280_students_and_examinations/script.sql
+++ src/main/kotlin/g1201_1300/s1280_students_and_examinations/script.sql
@@ -4,6 +4,6 @@ SELECT s.student_id, s.student_name, sub.subject_name,
 sum(case when sub.subject_name = e.subject_name then 1 else 0 end) as attended_exams
 FROM Students s
 cross join subjects sub
-left join examinations e on e.student_id= s.student_id
+left join examinations e on e.student_id=s.student_id
 group by 1,2,3
 order by 1,2,3
