cbe82df3a33f8b29c60316fcb732ab79b1a05d55
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2023-06-13 11:40:53 +0700

    Added tasks 1321, 1327, 1341

---
+++ src/test/kotlin/g1301_1400/s1327_list_the_products_ordered_in_a_period/MysqlTest.kt
@@ -0,0 +1,85 @@
+package g1301_1400.s1327_list_the_products_ordered_in_a_period
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
+import org.zapodot.junit.db.annotations.EmbeddedDatabase
+import org.zapodot.junit.db.annotations.EmbeddedDatabaseTest
+import org.zapodot.junit.db.common.CompatibilityMode
+import java.io.BufferedReader
+import java.io.FileNotFoundException
+import java.io.FileReader
+import java.sql.SQLException
+import java.util.stream.Collectors
+import javax.sql.DataSource
+
+@EmbeddedDatabaseTest(
+    compatibilityMode = CompatibilityMode.MySQL,
+    initialSqls = [
+        "CREATE TABLE Products(product_id INTEGER, product_name VARCHAR, product_category VARCHAR); " +
+            "INSERT INTO Products(product_id, product_name, product_category)" +
+            " VALUES (1, 'Leetcode Solutions', 'Book'); " +
+            "INSERT INTO Products(product_id, product_name, product_category)" +
+            " VALUES (2, 'Jewels of Stringology', 'Book'); " +
+            "INSERT INTO Products(product_id, product_name, product_category)" +
+            " VALUES (3, 'HP', 'Laptop'); " +
+            "INSERT INTO Products(product_id, product_name, product_category)" +
+            " VALUES (4, 'Lenovo', 'Laptop'); " +
+            "INSERT INTO Products(product_id, product_name, product_category)" +
+            " VALUES (5, 'Leetcode Kit', 'T-shirt'); " +
+            "CREATE TABLE Orders(product_id INTEGER, order_date DATE, unit INTEGER); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (1, '2020-02-05', 60); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (1, '2020-02-10', 70); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (2, '2020-01-18', 30); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (2, '2020-02-11', 80); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (3 , '2020-02-17', 2); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (3, '2020-02-24', 3); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (4, '2020-03-01', 20); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (4, '2020-03-04', 30); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (4, '2020-03-04', 60); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (5, '2020-02-25', 50); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (5, '2020-02-27', 50); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (5, '2020-03-01', 50); "
+    ]
+)
+internal class MysqlTest {
+    @Test
+    @Throws(SQLException::class, FileNotFoundException::class)
+    fun testScript(@EmbeddedDatabase dataSource: DataSource) {
+        dataSource.connection.use { connection ->
+            connection.createStatement().use { statement ->
+                statement.executeQuery(
+                    BufferedReader(
+                        FileReader(
+                            "src/main/kotlin/g1301_1400/s1327_list_the_products_ordered_in_a_period" +
+                                "/script.sql"
+                        )
+                    )
+                        .lines()
+                        .collect(Collectors.joining("\n"))
+                        .replace("#.*?\\r?\\n".toRegex(), "")
+                ).use { resultSet ->
+                    assertThat(resultSet.next(), equalTo(true))
+                    assertThat(resultSet.getNString(1), equalTo("Leetcode Kit"))
+                    assertThat(resultSet.getInt(2), equalTo(100))
+                    assertThat(resultSet.next(), equalTo(true))
+                    assertThat(resultSet.getNString(1), equalTo("Leetcode Solutions"))
+                    assertThat(resultSet.getInt(2), equalTo(130))
+                    assertThat(resultSet.next(), equalTo(false))
+                }
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/g1301_1400/s1341_movie_rating/MysqlTest.kt
@@ -0,0 +1,82 @@
+package g1301_1400.s1341_movie_rating
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
+import org.zapodot.junit.db.annotations.EmbeddedDatabase
+import org.zapodot.junit.db.annotations.EmbeddedDatabaseTest
+import org.zapodot.junit.db.common.CompatibilityMode
+import java.io.BufferedReader
+import java.io.FileNotFoundException
+import java.io.FileReader
+import java.sql.SQLException
+import java.util.stream.Collectors
+import javax.sql.DataSource
+
+@EmbeddedDatabaseTest(
+    compatibilityMode = CompatibilityMode.MySQL,
+    initialSqls = [
+        "CREATE TABLE Movies(movie_id INTEGER, title VARCHAR); " +
+            "INSERT INTO Movies(movie_id, title)" +
+            " VALUES (1, 'Avengers'); " +
+            "INSERT INTO Movies(movie_id, title)" +
+            " VALUES (2, 'Frozen 2'); " +
+            "INSERT INTO Movies(movie_id, title)" +
+            " VALUES (3, 'Joker'); " +
+            "CREATE TABLE Users(user_id INTEGER, name VARCHAR); " +
+            "INSERT INTO Users(user_id, name)" +
+            " VALUES (1, 'Daniel'); " +
+            "INSERT INTO Users(user_id, name)" +
+            " VALUES (2, 'Monica'); " +
+            "INSERT INTO Users(user_id, name)" +
+            " VALUES (3, 'Maria'); " +
+            "INSERT INTO Users(user_id, name)" +
+            " VALUES (4, 'James'); " +
+            "CREATE TABLE MovieRating(movie_id INTEGER, user_id INTEGER, rating INTEGER, created_at DATE); " +
+            "INSERT INTO MovieRating(movie_id, user_id, rating, created_at)" +
+            " VALUES (1, 1, 3, '2020-01-12'); " +
+            "INSERT INTO MovieRating(movie_id, user_id, rating, created_at)" +
+            " VALUES (1, 2, 4, '2020-02-11'); " +
+            "INSERT INTO MovieRating(movie_id, user_id, rating, created_at)" +
+            " VALUES (1, 3, 2, '2020-02-12'); " +
+            "INSERT INTO MovieRating(movie_id, user_id, rating, created_at)" +
+            " VALUES (1, 4, 1, '2020-01-01'); " +
+            "INSERT INTO MovieRating(movie_id, user_id, rating, created_at)" +
+            " VALUES (2, 1, 5, '2020-02-17'); " +
+            "INSERT INTO MovieRating(movie_id, user_id, rating, created_at)" +
+            " VALUES (2, 2, 2, '2020-02-01'); " +
+            "INSERT INTO MovieRating(movie_id, user_id, rating, created_at)" +
+            " VALUES (2, 3, 2, '2020-03-01'); " +
+            "INSERT INTO MovieRating(movie_id, user_id, rating, created_at)" +
+            " VALUES (3, 1, 3, '2020-02-22'); " +
+            "INSERT INTO MovieRating(movie_id, user_id, rating, created_at)" +
+            " VALUES (3, 2, 4, '2020-02-25'); "
+    ]
+)
+internal class MysqlTest {
+    @Test
+    @Throws(SQLException::class, FileNotFoundException::class)
+    fun testScript(@EmbeddedDatabase dataSource: DataSource) {
+        dataSource.connection.use { connection ->
+            connection.createStatement().use { statement ->
+                statement.executeQuery(
+                    BufferedReader(
+                        FileReader(
+                            "src/main/kotlin/g1301_1400/s1341_movie_rating" +
+                                "/script.sql"
+                        )
+                    )
+                        .lines()
+                        .collect(Collectors.joining("\n"))
+                        .replace("#.*?\\r?\\n".toRegex(), "")
+                ).use { resultSet ->
+                    assertThat(resultSet.next(), equalTo(true))
+                    assertThat(resultSet.getNString(1), equalTo("Daniel"))
+                    assertThat(resultSet.next(), equalTo(true))
+                    assertThat(resultSet.getNString(1), equalTo("Frozen 2"))
+                    assertThat(resultSet.next(), equalTo(false))
+                }
+            }
+        }
+    }
+}
