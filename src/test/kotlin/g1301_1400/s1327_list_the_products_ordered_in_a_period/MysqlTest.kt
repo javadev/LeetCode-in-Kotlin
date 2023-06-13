21c999479d11738bd2367bf72e59063992fcc906
ThanhNIT <thanhnguyen.it.work@gmail.com>
2023-06-13 08:30:53 +0700

    Update MysqlTest.kt

--- src/test/kotlin/g1301_1400/s1327_list_the_products_ordered_in_a_period/MysqlTest.kt
+++ src/test/kotlin/g1301_1400/s1327_list_the_products_ordered_in_a_period/MysqlTest.kt
@@ -17,41 +17,41 @@ import javax.sql.DataSource
     compatibilityMode = CompatibilityMode.MySQL,
     initialSqls = [
         "CREATE TABLE Products(product_id INTEGER, product_name VARCHAR, product_category VARCHAR); " +
-                "INSERT INTO Products(product_id, product_name, product_category)" +
-                " VALUES (1, 'Leetcode Solutions', 'Book'); " +
-                "INSERT INTO Products(product_id, product_name, product_category)" +
-                " VALUES (2, 'Jewels of Stringology', 'Book'); " +
-                "INSERT INTO Products(product_id, product_name, product_category)" +
-                " VALUES (3, 'HP', 'Laptop'); " +
-                "INSERT INTO Products(product_id, product_name, product_category)" +
-                " VALUES (4, 'Lenovo', 'Laptop'); " +
-                "INSERT INTO Products(product_id, product_name, product_category)" +
-                " VALUES (5, 'Leetcode Kit', 'T-shirt'); " +
-                "CREATE TABLE Orders(product_id INTEGER, order_date DATE, unit INTEGER); " +
-                "INSERT INTO Orders(product_id, order_date, unit)" +
-                " VALUES (1, '2020-02-05', 60); " +
-                "INSERT INTO Orders(product_id, order_date, unit)" +
-                " VALUES (1, '2020-02-10', 70); " +
-                "INSERT INTO Orders(product_id, order_date, unit)" +
-                " VALUES (2, '2020-01-18', 30); " +
-                "INSERT INTO Orders(product_id, order_date, unit)" +
-                " VALUES (2, '2020-02-11', 80); " +
-                "INSERT INTO Orders(product_id, order_date, unit)" +
-                " VALUES (3 , '2020-02-17', 2); " +
-                "INSERT INTO Orders(product_id, order_date, unit)" +
-                " VALUES (3, '2020-02-24', 3); " +
-                "INSERT INTO Orders(product_id, order_date, unit)" +
-                " VALUES (4, '2020-03-01', 20); " +
-                "INSERT INTO Orders(product_id, order_date, unit)" +
-                " VALUES (4, '2020-03-04', 30); " +
-                "INSERT INTO Orders(product_id, order_date, unit)" +
-                " VALUES (4, '2020-03-04', 60); " +
-                "INSERT INTO Orders(product_id, order_date, unit)" +
-                " VALUES (5, '2020-02-25', 50); " +
-                "INSERT INTO Orders(product_id, order_date, unit)" +
-                " VALUES (5, '2020-02-27', 50); " +
-                "INSERT INTO Orders(product_id, order_date, unit)" +
-                " VALUES (5, '2020-03-01', 50); "
+            "INSERT INTO Products(product_id, product_name, product_category)" +
+            " VALUES (1, 'Leetcode Solutions', 'Book'); " +
+            "INSERT INTO Products(product_id, product_name, product_category)" +
+            " VALUES (2, 'Jewels of Stringology', 'Book'); " +
+            "INSERT INTO Products(product_id, product_name, product_category)" +
+            " VALUES (3, 'HP', 'Laptop'); " +
+            "INSERT INTO Products(product_id, product_name, product_category)" +
+            " VALUES (4, 'Lenovo', 'Laptop'); " +
+            "INSERT INTO Products(product_id, product_name, product_category)" +
+            " VALUES (5, 'Leetcode Kit', 'T-shirt'); " +
+        "CREATE TABLE Orders(product_id INTEGER, order_date DATE, unit INTEGER); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (1, '2020-02-05', 60); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (1, '2020-02-10', 70); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (2, '2020-01-18', 30); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (2, '2020-02-11', 80); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (3 , '2020-02-17', 2); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (3, '2020-02-24', 3); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (4, '2020-03-01', 20); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (4, '2020-03-04', 30); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (4, '2020-03-04', 60); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (5, '2020-02-25', 50); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (5, '2020-02-27', 50); " +
+            "INSERT INTO Orders(product_id, order_date, unit)" +
+            " VALUES (5, '2020-03-01', 50); "
     ]
 )
 internal class MysqlTest {
