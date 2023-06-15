0c2d84387503eb5848c4230c497efa49c4f63237
Valentyn Kolesnikov <javadev75@gmail.com>
2023-06-15 15:27:11 +0300

    Fixed tests

--- src/test/kotlin/g1601_1700/s1617_count_subtrees_with_max_distance_between_cities/SolutionTest.kt
+++ src/test/kotlin/g1601_1700/s1617_count_subtrees_with_max_distance_between_cities/SolutionTest.kt
@@ -1,32 +1,32 @@
 package g1601_1700.s1617_count_subtrees_with_max_distance_between_cities
 
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 
 internal class SolutionTest {
     @Test
     fun countSubgraphsForEachDiameter() {
-        MatcherAssert.assertThat(
+        assertThat(
             Solution()
                 .countSubgraphsForEachDiameter(4, arrayOf(intArrayOf(1, 2), intArrayOf(2, 3), intArrayOf(2, 4))),
-            CoreMatchers.equalTo(intArrayOf(3, 4, 0))
+            equalTo(intArrayOf(3, 4, 0))
         )
     }
 
     @Test
     fun countSubgraphsForEachDiameter2() {
-        MatcherAssert.assertThat(
+        assertThat(
             Solution().countSubgraphsForEachDiameter(2, arrayOf(intArrayOf(1, 2))),
-            CoreMatchers.equalTo(intArrayOf(1))
+            equalTo(intArrayOf(1))
         )
     }
 
     @Test
     fun countSubgraphsForEachDiameter3() {
-        MatcherAssert.assertThat(
+        assertThat(
             Solution().countSubgraphsForEachDiameter(3, arrayOf(intArrayOf(1, 2), intArrayOf(2, 3))),
-            CoreMatchers.equalTo(intArrayOf(2, 1))
+            equalTo(intArrayOf(2, 1))
         )
     }
 }
