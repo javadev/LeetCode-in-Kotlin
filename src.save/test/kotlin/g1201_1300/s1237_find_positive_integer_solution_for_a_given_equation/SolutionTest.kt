231b8fd988bca04deef6223915d603a539d232f1
Valentyn Kolesnikov <javadev75@gmail.com>
2023-06-16 13:27:59 +0300

    Fixed test

--- src.save/test/kotlin/g1201_1300/s1237_find_positive_integer_solution_for_a_given_equation/SolutionTest.kt
+++ src.save/test/kotlin/g1201_1300/s1237_find_positive_integer_solution_for_a_given_equation/SolutionTest.kt
@@ -1,14 +1,13 @@
 package g1201_1300.s1237_find_positive_integer_solution_for_a_given_equation
 
-import g1201_1300.s1237_find_positive_integer_solution_for_a_given_equation.Solution.CustomFunction
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 
 internal class SolutionTest {
     @Test
-    fun findSolution {
-        val customFunction: CustomFunction = object : CustomFunction {
+    fun findSolution() {
+        val customFunction: Solution.CustomFunction = object : Solution.CustomFunction {
             var index = 0
             override fun f(x: Int, y: Int): Int {
                 return index++
