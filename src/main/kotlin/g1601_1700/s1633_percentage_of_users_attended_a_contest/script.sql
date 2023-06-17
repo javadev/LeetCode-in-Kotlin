fa66504f5abf879cc950fd9c9fea4851fdce75fd
ThanhNIT <thanhnguyen.it.work@gmail.com>
2023-06-17 11:26:26 +0700

    Update script.sql

--- src/main/kotlin/g1601_1700/s1633_percentage_of_users_attended_a_contest/script.sql
+++ src/main/kotlin/g1601_1700/s1633_percentage_of_users_attended_a_contest/script.sql
@@ -1,10 +1,9 @@
 # Write your MySQL query statement below
 # #Easy #Database #2023_06_17_Time_1556_ms_(93.74%)_Space_0B_(100.00%)
-with ucount as (
-  select count(user_id) as total from Users
-)
-Select contest_id,
-round((count(contest_id) / total)*100, 2) as percentage
-from Register, ucount
+select contest_id,
+round(count(contest_id)/(
+  select count(distinct user_id) from Users
+)*100.0,2) as percentage
+from Register
 group by contest_id
-order by percentage desc, contest_id asc;
+order by percentage desc, contest_id asc
