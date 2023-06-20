808066c41fb0293b4e798a7f8614d840543767a6
Valentyn Kolesnikov <javadev75@gmail.com>
2023-06-20 07:04:25 +0300

    Fixed test

--- src/test/kotlin/g1901_2000/s1932_merge_bsts_to_create_single_bst/SolutionTest.kt
+++ src/test/kotlin/g1901_2000/s1932_merge_bsts_to_create_single_bst/SolutionTest.kt
@@ -8,10 +8,10 @@ import org.junit.jupiter.api.Test
 internal class SolutionTest {
     @Test
     fun canMerge() {
-        val nodes: MutableList<TreeNode?> = mutableListOf(
-            TreeNode.create(mutableListOf(2, 1)),
-            TreeNode.create(mutableListOf(3, 2, 5)),
-            TreeNode.create(mutableListOf(5, 4))
+        val nodes: List<TreeNode> = listOf(
+            TreeNode.create(mutableListOf(2, 1))!!,
+            TreeNode.create(mutableListOf(3, 2, 5))!!,
+            TreeNode.create(mutableListOf(5, 4))!!
         )
         assertThat(
             Solution().canMerge(nodes).toString(),
@@ -21,9 +21,9 @@ internal class SolutionTest {
 
     @Test
     fun canMerge2() {
-        val nodes: MutableList<TreeNode?> = mutableListOf(
-            TreeNode.create(mutableListOf(5, 3, 8)),
-            TreeNode.create(mutableListOf(3, 2, 6))
+        val nodes: List<TreeNode> = listOf(
+            TreeNode.create(mutableListOf(5, 3, 8))!!,
+            TreeNode.create(mutableListOf(3, 2, 6))!!
         )
         assertThat(
             Solution().canMerge(nodes), equalTo(TreeNode.create(emptyList()))
@@ -32,8 +32,8 @@ internal class SolutionTest {
 
     @Test
     fun canMerge3() {
-        val nodes: MutableList<TreeNode?> = mutableListOf(
-            TreeNode.create(mutableListOf(5, 4)), TreeNode.create(mutableListOf(3))
+        val nodes: List<TreeNode> = listOf(
+            TreeNode.create(mutableListOf(5, 4))!!, TreeNode.create(mutableListOf(3))!!
         )
         assertThat(
             Solution().canMerge(nodes), equalTo(TreeNode.create(emptyList()))
