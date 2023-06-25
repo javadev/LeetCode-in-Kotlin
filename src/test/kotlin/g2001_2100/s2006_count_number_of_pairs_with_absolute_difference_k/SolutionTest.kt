3d74f620cbac1541c5d36e623e479c8ed70eca0b
Valentyn Kolesnikov <javadev75@gmail.com>
2023-06-25 06:56:12 +0300

    Update SolutionTest.kt

--- src/test/kotlin/g2001_2100/s2006_count_number_of_pairs_with_absolute_difference_k/SolutionTest.kt
+++ src/test/kotlin/g2001_2100/s2006_count_number_of_pairs_with_absolute_difference_k/SolutionTest.kt
@@ -1,22 +1,22 @@
 package g2001_2100.s2006_count_number_of_pairs_with_absolute_difference_k
 
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 
 internal class SolutionTest {
     @Test
     fun countKDifference() {
-        MatcherAssert.assertThat(Solution().countKDifference(intArrayOf(1, 2, 2, 1), 1), CoreMatchers.equalTo(4))
+        assertThat(Solution().countKDifference(intArrayOf(1, 2, 2, 1), 1), equalTo(4))
     }
 
     @Test
     fun countKDifference2() {
-        MatcherAssert.assertThat(Solution().countKDifference(intArrayOf(1, 3), 3), CoreMatchers.equalTo(0))
+        assertThat(Solution().countKDifference(intArrayOf(1, 3), 3), equalTo(0))
     }
 
     @Test
     fun countKDifference3() {
-        MatcherAssert.assertThat(Solution().countKDifference(intArrayOf(3, 2, 1, 5, 4), 2), CoreMatchers.equalTo(3))
+        assertThat(Solution().countKDifference(intArrayOf(3, 2, 1, 5, 4), 2), equalTo(3))
     }
 }
