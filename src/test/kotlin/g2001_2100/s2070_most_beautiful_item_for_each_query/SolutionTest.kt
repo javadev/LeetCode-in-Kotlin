1c89c675ca422903bb2909b0ee750b6af0272392
Valentyn Kolesnikov <javadev75@gmail.com>
2023-06-26 09:20:50 +0300

    Update SolutionTest.kt

--- src/test/kotlin/g2001_2100/s2070_most_beautiful_item_for_each_query/SolutionTest.kt
+++ src/test/kotlin/g2001_2100/s2070_most_beautiful_item_for_each_query/SolutionTest.kt
@@ -1,7 +1,7 @@
 package g2001_2100.s2070_most_beautiful_item_for_each_query
 
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 
 internal class SolutionTest {
@@ -10,7 +10,7 @@ internal class SolutionTest {
         val items = arrayOf(intArrayOf(1, 2), intArrayOf(3, 2), intArrayOf(2, 4), intArrayOf(5, 6), intArrayOf(3, 5))
         val queries = intArrayOf(1, 2, 3, 4, 5, 6)
         val expected = intArrayOf(2, 4, 5, 5, 6, 6)
-        MatcherAssert.assertThat(Solution().maximumBeauty(items, queries), CoreMatchers.equalTo(expected))
+        assertThat(Solution().maximumBeauty(items, queries), equalTo(expected))
     }
 
     @Test
@@ -18,7 +18,7 @@ internal class SolutionTest {
         val items = arrayOf(intArrayOf(1, 2), intArrayOf(1, 2), intArrayOf(1, 3), intArrayOf(1, 4))
         val queries = intArrayOf(1)
         val expected = intArrayOf(4)
-        MatcherAssert.assertThat(Solution().maximumBeauty(items, queries), CoreMatchers.equalTo(expected))
+        assertThat(Solution().maximumBeauty(items, queries), equalTo(expected))
     }
 
     @Test
@@ -26,6 +26,6 @@ internal class SolutionTest {
         val items = arrayOf(intArrayOf(10, 1000))
         val queries = intArrayOf(5)
         val expected = intArrayOf(0)
-        MatcherAssert.assertThat(Solution().maximumBeauty(items, queries), CoreMatchers.equalTo(expected))
+        assertThat(Solution().maximumBeauty(items, queries), equalTo(expected))
     }
 }
