44d6517fce2967b7dbacfc620b27f8492706ef4d
Valentyn Kolesnikov <javadev75@gmail.com>
2023-06-27 09:26:10 +0300

    Improved task 430

--- src/main/kotlin/g0401_0500/s0430_flatten_a_multilevel_doubly_linked_list/Node.kt
+++ src/main/kotlin/g0401_0500/s0430_flatten_a_multilevel_doubly_linked_list/Node.kt
@@ -1,12 +1,6 @@
 package g0401_0500.s0430_flatten_a_multilevel_doubly_linked_list
 
 class Node(var `val`: Int) {
-    constructor(i: Int, node: Node?, node1: Node?, nothing: Node?) : this(i) {
-        this.prev = node
-        this.next = node1
-        this.child = nothing
-    }
-
     var prev: Node? = null
     var next: Node? = null
     var child: Node? = null
