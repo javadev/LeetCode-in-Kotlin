7b606908cfe39c6aff874106c215dde95f5e1a8f
ThanhNIT <thanhnguyen.it.work@gmail.com>
2023-06-28 08:52:34 +0700

    Update Solution.kt

--- src/main/kotlin/g2201_2300/s2255_count_prefixes_of_a_given_string/Solution.kt
+++ src/main/kotlin/g2201_2300/s2255_count_prefixes_of_a_given_string/Solution.kt
@@ -3,10 +3,10 @@ package g2201_2300.s2255_count_prefixes_of_a_given_string
 // #Easy #Array #String #2023_06_28_Time_178_ms_(83.33%)_Space_36.5_MB_(100.00%)
 
 class Solution {
-    fun countPrefixes(words: Array<String?>, s: String): Int {
+    fun countPrefixes(words: Array<String>, s: String): Int {
         var count = 0
         for (str in words) {
-            if (s.indexOf(str!!) == 0) {
+            if (s.indexOf(str) == 0) {
                 ++count
             }
         }
