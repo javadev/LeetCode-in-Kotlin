0df6de89897d2c9735990eeb99f1b340d4b9b0f7
ThanhNIT <thanhnguyen.it.work@gmail.com>
2023-07-12 08:20:10 +0700

    Update Solution.kt

--- src/main/kotlin/g2501_2600/s2591_distribute_money_to_maximum_children/Solution.kt
+++ src/main/kotlin/g2501_2600/s2591_distribute_money_to_maximum_children/Solution.kt
@@ -7,9 +7,8 @@ class Solution {
     fun distMoney(money: Int, children: Int): Int {
         var money = money
         if (children > money) return -1
-        var ans = 0
         money -= children
-        ans = money / 7
+        var ans: Int = money / 7
         if (ans == children && money % 7 == 0) return ans
         if (ans >= children) return children - 1
         if (money % 7 == 3 && ans == children - 1) ans--
