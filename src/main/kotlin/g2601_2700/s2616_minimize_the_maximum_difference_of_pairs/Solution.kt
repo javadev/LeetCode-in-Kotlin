6e17ddb2d17a7f7747bfbc5a3e2016021c8d938c
Valentyn Kolesnikov <javadev75@gmail.com>
2023-07-14 02:36:04 +0300

    Fixed compile

--- src/main/kotlin/g2601_2700/s2616_minimize_the_maximum_difference_of_pairs/Solution.kt
+++ src/main/kotlin/g2601_2700/s2616_minimize_the_maximum_difference_of_pairs/Solution.kt
@@ -2,6 +2,7 @@ package g2601_2700.s2616_minimize_the_maximum_difference_of_pairs
 
 // #Medium #Array #Greedy #Binary_Search #2023_07_14_Time_556_ms_(100.00%)_Space_64_MB_(100.00%)
 
+@Suppress("NAME_SHADOWING")
 class Solution {
     private fun ispossible(nums: IntArray, p: Int, diff: Int): Boolean {
         var p = p
