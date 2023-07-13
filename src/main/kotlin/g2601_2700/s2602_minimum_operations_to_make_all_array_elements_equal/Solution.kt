d942391b3af6ae4b45ddc15434943d85693a2a05
Valentyn Kolesnikov <javadev75@gmail.com>
2023-07-13 05:36:16 +0300

    Update Solution.kt

--- src/main/kotlin/g2601_2700/s2602_minimum_operations_to_make_all_array_elements_equal/Solution.kt
+++ src/main/kotlin/g2601_2700/s2602_minimum_operations_to_make_all_array_elements_equal/Solution.kt
@@ -3,9 +3,6 @@ package g2601_2700.s2602_minimum_operations_to_make_all_array_elements_equal
 // #Medium #Array #Sorting #Binary_Search #Prefix_Sum
 // #2023_07_13_Time_790_ms_(100.00%)_Space_63.8_MB_(100.00%)
 
-import java.util.ArrayList
-import java.util.Arrays
-
 class Solution {
     fun minOperations(nums: IntArray, queries: IntArray): List<Long> {
         nums.sort()
@@ -36,7 +33,7 @@ class Solution {
     }
 
     private fun getIndex(nums: IntArray, target: Int): Int {
-        var index = Arrays.binarySearch(nums, target)
+        var index = nums.binarySearch(target)
         if (index < 0) index = -(index + 1)
         if (index == nums.size) --index
         return index
