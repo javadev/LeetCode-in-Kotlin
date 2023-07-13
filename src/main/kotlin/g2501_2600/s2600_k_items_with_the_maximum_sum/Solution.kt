2b646235f588013eec77f4e8f8e6061313be89dc
Valentyn Kolesnikov <javadev75@gmail.com>
2023-07-13 06:39:21 +0300

    Update Solution.kt

--- src/main/kotlin/g2501_2600/s2600_k_items_with_the_maximum_sum/Solution.kt
+++ src/main/kotlin/g2501_2600/s2600_k_items_with_the_maximum_sum/Solution.kt
@@ -2,8 +2,9 @@ package g2501_2600.s2600_k_items_with_the_maximum_sum
 
 // #Easy #Math #Greedy #2023_07_13_Time_145_ms_(100.00%)_Space_33.5_MB_(75.00%)
 
+@Suppress("UNUSED_PARAMETER")
 class Solution {
-    fun kItemsWithMaximumSum(numOnes: Int, numZeros: Int, k: Int): Int {
+    fun kItemsWithMaximumSum(numOnes: Int, numZeros: Int, numNegOnes: Int, k: Int): Int {
         if (k <= numOnes) {
             return k
         }
