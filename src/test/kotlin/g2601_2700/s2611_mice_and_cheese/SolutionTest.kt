ff94a3452f387c1a6ca9c4d3e8e1df9a22d8c3e5
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2023-07-15 11:09:06 +0700

    Added task 2611

---
+++ src/test/kotlin/g2601_2700/s2611_mice_and_cheese/SolutionTest.kt
@@ -0,0 +1,27 @@
+package g2601_2700.s2611_mice_and_cheese
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
+
+internal class SolutionTest {
+    @Test
+    fun miceAndCheese() {
+        assertThat(
+            Solution().miceAndCheese(intArrayOf(1, 1, 3, 4), intArrayOf(4, 4, 1, 1), 2),
+            equalTo(
+                15
+            )
+        )
+    }
+
+    @Test
+    fun miceAndCheese2() {
+        assertThat(
+            Solution().miceAndCheese(intArrayOf(1, 1), intArrayOf(1, 1), 2),
+            equalTo(
+                2
+            )
+        )
+    }
+}
