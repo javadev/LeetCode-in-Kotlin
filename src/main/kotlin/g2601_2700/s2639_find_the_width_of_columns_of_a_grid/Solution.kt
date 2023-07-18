314a338a993e6d5ec26af50c63e65549dfc2735c
Valentyn Kolesnikov <javadev75@gmail.com>
2023-07-18 05:23:10 +0300

    Update Solution.kt

--- src/main/kotlin/g2601_2700/s2639_find_the_width_of_columns_of_a_grid/Solution.kt
+++ src/main/kotlin/g2601_2700/s2639_find_the_width_of_columns_of_a_grid/Solution.kt
@@ -5,33 +5,27 @@ package g2601_2700.s2639_find_the_width_of_columns_of_a_grid
 class Solution {
     fun findColumnWidth(grid: Array<IntArray>): IntArray {
         val ans = IntArray(grid[0].size) { 1 }
-
         for (j in 0 until grid[0].size) {
             var max = 1
             for (i in 0 until grid.size) {
                 var num = grid[i][j]
                 var neg = false
-
                 if (num < 0) {
                     neg = true
                     num *= -1
                 }
-
                 var size = 0
                 while (num > 0) {
                     num /= 10
                     size++
                 }
-
                 if (neg) {
                     size += 1
                 }
-
                 max = maxOf(max, size)
             }
             ans[j] = max
         }
-
         return ans
     }
 }
