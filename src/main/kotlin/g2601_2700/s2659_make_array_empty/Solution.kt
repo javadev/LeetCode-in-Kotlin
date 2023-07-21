c8b758cf6ef64c974885b68a5b636e8a53fe5bea
Valentyn Kolesnikov <javadev75@gmail.com>
2023-07-21 05:27:16 +0300

    Update Solution.kt

--- src/main/kotlin/g2601_2700/s2659_make_array_empty/Solution.kt
+++ src/main/kotlin/g2601_2700/s2659_make_array_empty/Solution.kt
@@ -6,19 +6,16 @@ package g2601_2700.s2659_make_array_empty
 class Solution {
     fun countOperationsToEmptyArray(nums: IntArray): Long {
         val sortNums = Array(nums.size) { IntArray(2) }
-
         for (i in nums.indices) {
             sortNums[i][0] = nums[i]
             sortNums[i][1] = i
         }
         sortNums.sortBy { it[0] }
-
         var res = 0L + nums.size
         for (i in 1..sortNums.lastIndex) {
             if (sortNums[i - 1][1] > sortNums[i][1])
                 res += nums.size - i
         }
-
         return res
     }
 }
