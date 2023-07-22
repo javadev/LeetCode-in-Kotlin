75dc31ebd5fafd45773610ab606d3e1e3358ccfb
Valentyn Kolesnikov <javadev75@gmail.com>
2023-07-22 03:58:28 +0300

    Fixed compile

--- src/main/kotlin/g0001_0100/s0025_reverse_nodes_in_k_group/Solution.kt
+++ src/main/kotlin/g0001_0100/s0025_reverse_nodes_in_k_group/Solution.kt
@@ -2,7 +2,7 @@ package g0001_0100.s0025_reverse_nodes_in_k_group
 
 // #Hard #Top_100_Liked_Questions #Linked_List #Recursion #Data_Structure_II_Day_13_Linked_List
 // #Udemy_Linked_List #2023_07_03_Time_189_ms_(67.03%)_Space_36.7_MB_(71.43%)
-9
+
 import com_github_leetcode.ListNode
 
 /*
