15a8f6762f696ab735c2385a1a0683546877216f
Valentyn Kolesnikov <javadev75@gmail.com>
2023-07-27 05:17:27 +0300

    Update solution.test.ts

--- src/test/kotlin/g2601_2700/s2677_chunk_array/solution.test.ts
+++ src/test/kotlin/g2601_2700/s2677_chunk_array/solution.test.ts
@@ -6,14 +6,14 @@ test('chunk', () => {
     expect(chunk([1,2,3,4,5], 1)).toEqual([[1],[2],[3],[4],[5]])
 })
 
-test('chunk', () => {
+test('chunk2', () => {
     expect(chunk([1,9,6,3,2], 3)).toEqual([[1,9,6],[3,2]])
 })
 
-test('chunk', () => {
+test('chunk3', () => {
     expect(chunk([8,5,3,2,6], 6)).toEqual([[8,5,3,2,6]])
 })
 
-test('chunk', () => {
+test('chunk4', () => {
     expect(chunk([], 1)).toEqual([])
 })
