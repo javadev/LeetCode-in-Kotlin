1826fd228cc679bfbf2d36df8b4a78e21f975d8d
Valentyn Kolesnikov <javadev75@gmail.com>
2023-07-28 07:04:43 +0300

    Update Solution.kt

--- src/main/kotlin/g2601_2700/s2685_count_the_number_of_complete_components/Solution.kt
+++ src/main/kotlin/g2601_2700/s2685_count_the_number_of_complete_components/Solution.kt
@@ -1,67 +1,38 @@
 package g2601_2700.s2685_count_the_number_of_complete_components
 
-// #Medium #Graph #Depth_First_Search #Breadth_First_Search
-// #2023_07_28_Time_378_ms_(100.00%)_Space_58.3_MB_(66.67%)
+// #Medium #Array #Dynamic_Programming #Matrix #Graph #Depth_First_Search #Breadth_First_Search
+// #2023_07_28_Time_436_ms_(100.00%)_Space_58.6_MB_(66.67%)
 
 @Suppress("NAME_SHADOWING")
 class Solution {
-    private lateinit var parent: IntArray
-    private lateinit var rank: IntArray
-    private lateinit var edgeCount: IntArray
-    private lateinit var verticesCount: IntArray
-
-    private fun find(i: Int): Int {
-        var i = i
-        if (parent[i] != i) {
-            parent[i] = parent[parent[i]]
-            i = parent[i]
-        }
-        return i
-    }
-
-    private fun union(edge: IntArray) {
-        val p1 = find(edge[0])
-        val p2 = find(edge[1])
-        edgeCount[p1]++
-        if (p1 == p2) {
-            return
-        }
-        if (rank[p1] < rank[p2]) {
-            parent[p1] = p2
-            edgeCount[p2] += edgeCount[p1]
-            verticesCount[p2] += verticesCount[p1]
-        } else {
-            parent[p2] = p1
-            edgeCount[p1] += edgeCount[p2]
-            verticesCount[p1] += verticesCount[p2]
-            if (rank[p1] == rank[p2]) {
-                rank[p1]++
-            }
-        }
-    }
-
-    private fun init(n: Int) {
-        edgeCount = IntArray(n)
-        rank = IntArray(n)
-        verticesCount = IntArray(n)
-        verticesCount.fill(1)
-        parent = IntArray(n)
-        for (i in 0 until n) {
-            parent[i] = i
-        }
-    }
-
     fun countCompleteComponents(n: Int, edges: Array<IntArray>): Int {
-        init(n)
-        for (edge in edges) {
-            union(edge)
-        }
-        var count = 0
-        for (i in 0 until n) {
-            if (parent[i] == i && edgeCount[i] == verticesCount[i] * (verticesCount[i] - 1) / 2) {
-                count++
+        val adj = HashMap<Int, ArrayList<Int>>().apply {
+            for ((u, v) in edges) {
+                this[u] = getOrDefault(u, arrayListOf()).apply { add(v) }
+                this[v] = getOrDefault(v, arrayListOf()).apply { add(u) }
             }
         }
-        return count
+        val visited = BooleanArray(n)
+        fun bfs(i: Int): Pair<Int, Int> {
+            if (visited[i]) return 0 to 0
+            visited[i] = true
+            var nodes = 1
+            var edges = (adj[i]?.size ?: 0)
+            adj[i]?.forEach {
+                val (nodes2, edges2) = bfs(it)
+                nodes += nodes2
+                edges += edges2
+            }
+            return nodes to edges
+        }
+        var res = 0
+        for (i in 0 until n) {
+            if (!visited[i]) {
+                val (nodes, edges) = bfs(i)
+                if ((nodes * (nodes - 1)) == edges)
+                    res++
+            }
+        }
+        return res
     }
 }
