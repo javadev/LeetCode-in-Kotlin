6590ae0cbb52c0ac2674fc17d4e4a68c9fed55de
Valentyn Kolesnikov <javadev75@gmail.com>
2023-07-31 06:24:48 +0300

    Update SolutionTest.kt

--- src/test/kotlin/g2701_2800/s2708_maximum_strength_of_a_group/SolutionTest.kt
+++ src/test/kotlin/g2701_2800/s2708_maximum_strength_of_a_group/SolutionTest.kt
@@ -20,4 +20,12 @@ internal class SolutionTest {
             equalTo(20)
         )
     }
+
+    @Test
+    fun maxStrength3() {
+        assertThat(
+            Solution().maxStrength(intArrayOf(8, 6, 0, 5, -4, -8, -4, 9, -1, 6, -4, 8, -5)),
+            equalTo(265420800)
+        )
+    }
 }
