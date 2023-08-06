5af50b53d20a97d6d6184640a05d66f6fa9a36d8
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-06 08:23:34 +0300

    Improved tasks

--- src/main/kotlin/g2701_2800/s2724_sort_by/solution.ts
+++ src/main/kotlin/g2701_2800/s2724_sort_by/solution.ts
@@ -1,7 +1,9 @@
 // #Easy #2023_08_03_Time_132_ms_(96.70%)_Space_56.9_MB_(73.28%)
 
 function sortBy(arr: any[], fn: Function): any[] {
-    let swap: any = (a: any, b: any)=>{return (fn(a) < fn(b))?-1:1;};
+    let swap: any = (a: any, b: any) => {
+        return fn(a) < fn(b) ? -1 : 1
+    }
     return arr.sort(swap)
 }
 
--- src/main/kotlin/g2701_2800/s2727_is_object_empty/solution.ts
+++ src/main/kotlin/g2701_2800/s2727_is_object_empty/solution.ts
@@ -2,6 +2,6 @@
 
 function isEmpty(obj: Record<string, any> | any[]): boolean {
     return Object.keys(obj).length === 0
-};
+}
 
 export { isEmpty }
