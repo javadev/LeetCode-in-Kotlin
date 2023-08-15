e50181c8e945536c3d180777627fa47c89e0ed27
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-15 06:38:15 +0300

    Improved task 1341

--- src/main/kotlin/g1301_1400/s1341_movie_rating/script.sql
+++ src/main/kotlin/g1301_1400/s1341_movie_rating/script.sql
@@ -1,20 +1,13 @@
 # Write your MySQL query statement below
-# #Medium #Database #2023_06_13_Time_2387_ms_(59.80%)_Space_0B_(100.00%)
-with cte as
-(SELECT name, COUNT(RATING) as cnt,
-DENSE_RANK() over(order by COUNT(RATING) desc, name asc) as rnk FROM MovieRating r
-INNER JOIN Users u ON r.user_id = u.user_id
-GROUP BY u.user_id
-limit 1),
-
-cte2 as
-(SELECT title, avg(rating) as avgr from MovieRating r
-INNER JOIN Movies m ON r.movie_id = m.movie_id
-where month(created_at) = 2
-group by r.movie_id
-order by avg(rating) desc, title asc
-limit 1)
-
-select name as results from cte
-union all
-select title from cte2
+# #Medium #Database #2023_08_15_Time_2843_ms_(48.31%)_Space_0B_(100.00%)
+(SELECT name results
+FROM Users as U, MovieRating as  MR
+WHERE U.user_id = MR.user_id
+GROUP BY U.user_id
+ORDER BY COUNT(MR.user_id) DESC, name ASC LIMIT 1)
+UNION ALL
+(SELECT title results
+FROM Movies as M, MovieRating as MR
+WHERE M.movie_id = MR.movie_id AND created_at BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY M.movie_id
+ORDER BY AVG(rating) DESC, title ASC LIMIT 1)
