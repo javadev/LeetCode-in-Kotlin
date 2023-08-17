547e46005a01e61c8bb1a88e5d01ec034f745f1d
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-17 05:56:24 +0300

    Update script.sql

--- src/main/kotlin/g1701_1800/s1789_primary_department_for_each_employee/script.sql
+++ src/main/kotlin/g1701_1800/s1789_primary_department_for_each_employee/script.sql
@@ -1,6 +1,14 @@
 # Write your MySQL query statement below
-# #Easy #Database #2023_06_18_Time_1033_ms_(77.87%)_Space_0B_(100.00%)
-select e1.employee_id, case when e2.department_id is null then e1.department_id else e2.department_id end as department_id
-from employee e1
-left join (select * from employee where primary_flag = 'Y')e2 on e1.employee_id = e2.employee_id
-group by e1.employee_id, department_id
+# #Easy #Database #2023_08_16_Time_1122_ms_(74.47%)_Space_0B_(100.00%)
+WITH cte AS (
+    SELECT DISTINCT employee_id, department_id,
+           COUNT(employee_id) OVER (PARTITION BY employee_id) AS n
+    FROM Employee
+)
+SELECT employee_id, department_id
+FROM cte
+WHERE n = 1
+UNION
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y';
