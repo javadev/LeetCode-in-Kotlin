103fefbf4da98b14c4d2844eade5f185ac4220db
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-17 05:53:15 +0300

    Update script.sql

--- src/main/kotlin/g1901_2000/s1907_count_salary_categories/script.sql
+++ src/main/kotlin/g1901_2000/s1907_count_salary_categories/script.sql
@@ -1,11 +1,7 @@
 # Write your MySQL query statement below
-# #Medium #Database #2023_06_22_Time_2358_ms_(73.89%)_Space_0B_(100.00%)
-with cte1 as (SELECT 'Low Salary' AS category,SUM(CASE WHEN INCOME<20000 THEN 1 ELSE 0 END ) AS accounts_count from Accounts),
-cte2 as(SELECT 'Average Salary' AS category,SUM(CASE WHEN INCOME BETWEEN 20000 AND 50000 THEN 1 ELSE 0 END ) AS accounts_count from Accounts),
-cte3 as (SELECT 'High Salary' AS category,SUM(CASE WHEN INCOME> 50000 THEN  1 ELSE 0 END) AS accounts_count
-FROM Accounts)
-SELECT * from cte3
-UNION ALL
-SELECT * FROM cte1
-UNION ALL
-SELECT * FROM cte2
+# #Medium #Database #2023_08_17_Time_2283_ms_(89.29%)_Space_0B_(100.00%)
+select 'Low Salary' as category, count(*) as accounts_count from accounts where income<20000
+union
+select 'Average Salary' as category, count(*) as accounts_count from accounts where income>=20000 and income<=50000
+union
+select 'High Salary' as category, count(*) as accounts_count from accounts where income>50000
