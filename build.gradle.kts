d7d5ea836912b48d131b68eea23e174b592bc92b
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-30 21:28:00 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.10"
     jacoco
     id("org.sonarqube") version "4.3.0.3225"
     id("com.diffplug.spotless") version "6.12.0"
