c695902daeee8738de41226c2746972a6d0c26ae
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-19 21:04:22 +0300

    Improved task 2684

--- src/main/kotlin/g2601_2700/s2684_maximum_number_of_moves_in_a_grid/Solution.kt
+++ src/main/kotlin/g2601_2700/s2684_maximum_number_of_moves_in_a_grid/Solution.kt
@@ -1,29 +1,40 @@
 package g2601_2700.s2684_maximum_number_of_moves_in_a_grid
 
 // #Medium #Array #Dynamic_Programming #Matrix
-// #2023_07_28_Time_542_ms_(100.00%)_Space_57.4_MB_(57.14%)
+// #2023_09_19_Time_509_ms_(100.00%)_Space_67.9_MB_(25.00%)
 
 class Solution {
     fun maxMoves(grid: Array<IntArray>): Int {
-        val height = grid.size
-        val width = grid[0].size
-        val dp = Array(height) { IntArray(width) { Int.MIN_VALUE } }
-        var result = 0
-        for (i in 0 until height) {
-            dp[i][0] = 0
-        }
-        for (c in 1 until width) {
-            for (r in 0 until height) {
-                if (r > 0 && grid[r - 1][c - 1] < grid[r][c]) {
-                    dp[r][c] = dp[r][c].coerceAtLeast(dp[r - 1][c - 1] + 1)
+        val h = grid.size
+        var dp1 = BooleanArray(h)
+        var dp2 = BooleanArray(h)
+        var rtn = 0
+        dp1.fill(true)
+        for (col in 1 until grid[0].size) {
+            var f = false
+            for (row in 0 until h) {
+                val pr = row - 1
+                val nr = row + 1
+                dp2[row] = false
+                if (pr >= 0 && dp1[pr] && grid[pr][col - 1] < grid[row][col]) {
+                    dp2[row] = true
+                    f = true
                 }
-                if (grid[r][c - 1] < grid[r][c]) dp[r][c] = dp[r][c].coerceAtLeast(dp[r][c - 1] + 1)
-                if (r < height - 1 && grid[r + 1][c - 1] < grid[r][c]) {
-                    dp[r][c] = dp[r][c].coerceAtLeast(dp[r + 1][c - 1] + 1)
+                if (nr < h && dp1[nr] && grid[nr][col - 1] < grid[row][col]) {
+                    dp2[row] = true
+                    f = true
                 }
-                result = result.coerceAtLeast(dp[r][c])
+                if (dp1[row] && grid[row][col - 1] < grid[row][col]) {
+                    dp2[row] = true
+                    f = true
+                }
             }
+            val t = dp1
+            dp1 = dp2
+            dp2 = t
+            if (!f) break
+            rtn++
         }
-        return result
+        return rtn
     }
 }
