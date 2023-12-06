8d0716eb0fa9c89e8dcb077cd3f1413ebd4b8f3e
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-06 19:43:05 +0200

    Improved task 2824

--- src/main/kotlin/g2801_2900/s2824_count_pairs_whose_sum_is_less_than_target/Solution.kt
+++ src/main/kotlin/g2801_2900/s2824_count_pairs_whose_sum_is_less_than_target/Solution.kt
@@ -1,6 +1,7 @@
 package g2801_2900.s2824_count_pairs_whose_sum_is_less_than_target
 
-// #Easy #Array #Sorting #Two_Pointers
+// #Easy #Array #Sorting #Two_Pointers #2023_12_06_Time_168_ms_(90.62%)_Space_35.1_MB_(92.71%)
+
 class Solution {
     fun countPairs(nums: List<Int>, target: Int): Int {
         var cnt = 0
