441c8662d18561bd861987224941c05ce06cf191
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-25 13:31:56 +0200

    Improved tests

--- src/test/kotlin/g2801_2900/s2874_maximum_value_of_an_ordered_triplet_ii/SolutionTest.kt
+++ src/test/kotlin/g2801_2900/s2874_maximum_value_of_an_ordered_triplet_ii/SolutionTest.kt
@@ -1,25 +1,25 @@
 package g2801_2900.s2874_maximum_value_of_an_ordered_triplet_ii
 
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 
 internal class SolutionTest {
     @Test
     fun maximumTripletValue() {
-        MatcherAssert.assertThat(Solution().maximumTripletValue(intArrayOf(12, 6, 1, 2, 7)), CoreMatchers.equalTo(77L))
+        assertThat(Solution().maximumTripletValue(intArrayOf(12, 6, 1, 2, 7)), equalTo(77L))
     }
 
     @Test
     fun maximumTripletValue2() {
-        MatcherAssert.assertThat(
+        assertThat(
             Solution().maximumTripletValue(intArrayOf(1, 10, 3, 4, 19)),
-            CoreMatchers.equalTo(133L)
+            equalTo(133L)
         )
     }
 
     @Test
     fun maximumTripletValue3() {
-        MatcherAssert.assertThat(Solution().maximumTripletValue(intArrayOf(1, 2, 3)), CoreMatchers.equalTo(0L))
+        assertThat(Solution().maximumTripletValue(intArrayOf(1, 2, 3)), equalTo(0L))
     }
 }
--- src/test/kotlin/g2801_2900/s2894_divisible_and_non_divisible_sums_difference/SolutionTest.kt
+++ src/test/kotlin/g2801_2900/s2894_divisible_and_non_divisible_sums_difference/SolutionTest.kt
@@ -1,22 +1,22 @@
 package g2801_2900.s2894_divisible_and_non_divisible_sums_difference
 
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 
 internal class SolutionTest {
     @Test
     fun differenceOfSums() {
-        MatcherAssert.assertThat(Solution().differenceOfSums(10, 3), CoreMatchers.equalTo(19))
+        assertThat(Solution().differenceOfSums(10, 3), equalTo(19))
     }
 
     @Test
     fun differenceOfSums2() {
-        MatcherAssert.assertThat(Solution().differenceOfSums(5, 6), CoreMatchers.equalTo(15))
+        assertThat(Solution().differenceOfSums(5, 6), equalTo(15))
     }
 
     @Test
     fun differenceOfSums3() {
-        MatcherAssert.assertThat(Solution().differenceOfSums(5, 1), CoreMatchers.equalTo(-15))
+        assertThat(Solution().differenceOfSums(5, 1), equalTo(-15))
     }
 }
