a4f51ffe1c95c320b56816a8354ea5554e28ad5f
Valentyn Kolesnikov <javadev75@gmail.com>
2024-03-30 09:51:14 +0200

    Improved task 1890

--- src/main/kotlin/g1801_1900/s1890_the_latest_login_in_2020/script.sql
+++ src/main/kotlin/g1801_1900/s1890_the_latest_login_in_2020/script.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-# #Easy #Database #SQL_I_Day_8_Function #2023_06_22_Time_1486_ms_(22.18%)_Space_0B_(100.00%)
+# #Easy #Database #SQL_I_Day_8_Function #2024_03_30_Time_1280_ms_(43.62%)_Space_0B_(100.00%)
 SELECT user_id, MAX(time_stamp) AS last_stamp
 FROM Logins
 WHERE YEAR(time_stamp) = '2020'
