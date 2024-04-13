82f7d84460c89cdfb92054621a529fa6ecf37091
Valentyn Kolesnikov <javadev75@gmail.com>
2024-04-13 04:47:10 +0300

    Fixed test

--- src/test/kotlin/g3101_3200/s3106_lexicographically_smallest_string_after_operations_with_constraint/SolutionTest.kt
+++ src/test/kotlin/g3101_3200/s3106_lexicographically_smallest_string_after_operations_with_constraint/SolutionTest.kt
@@ -1,22 +1,22 @@
 package g3101_3200.s3106_lexicographically_smallest_string_after_operations_with_constraint
 
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 
 internal class SolutionTest {
     @Test
     fun smallestString() {
-        MatcherAssert.assertThat(Solution().getSmallestString("zbbz", 3), CoreMatchers.equalTo("aaaz"))
+        assertThat(Solution().getSmallestString("zbbz", 3), equalTo("aaaz"))
     }
 
     @Test
     fun smallestString2() {
-        MatcherAssert.assertThat(Solution().getSmallestString("xaxcd", 4), CoreMatchers.equalTo("aawcd"))
+        assertThat(Solution().getSmallestString("xaxcd", 4), equalTo("aawcd"))
     }
 
     @Test
     fun smallestString3() {
-        MatcherAssert.assertThat(Solution().getSmallestString("lol", 0), CoreMatchers.equalTo("lol"))
+        assertThat(Solution().getSmallestString("lol", 0), equalTo("lol"))
     }
 }
