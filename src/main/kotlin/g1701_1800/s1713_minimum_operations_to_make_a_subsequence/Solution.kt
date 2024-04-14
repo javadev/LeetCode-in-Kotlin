da8d9198775ccc04a0f8baa6efc3585676d020f0
Valentyn Kolesnikov <javadev75@gmail.com>
2024-04-14 05:44:46 +0300

    Fixed solution

--- src/main/kotlin/g1701_1800/s1713_minimum_operations_to_make_a_subsequence/Solution.kt
+++ src/main/kotlin/g1701_1800/s1713_minimum_operations_to_make_a_subsequence/Solution.kt
@@ -3,6 +3,8 @@ package g1701_1800.s1713_minimum_operations_to_make_a_subsequence
 // #Hard #Array #Hash_Table #Greedy #Binary_Search
 // #2023_06_16_Time_862_ms_(100.00%)_Space_67.3_MB_(100.00%)
 
+import java.util.Arrays
+
 class Solution {
     fun minOperations(target: IntArray, arr: IntArray): Int {
         val map: MutableMap<Int, Int> = HashMap()
@@ -23,7 +25,7 @@ class Solution {
         var l = 0
         val arr = IntArray(n)
         for (num in list) {
-            var index = arr.binarySearch(0, l, num!!)
+            var index = Arrays.binarySearch(arr, 0, l, num!!)
             if (index < 0) {
                 index = index.inv()
             }
