a09fc0bda9c975c32ad405cf4b3236586b25a692
Valentyn Kolesnikov <javadev75@gmail.com>
2024-04-28 10:24:35 +0300

    Improved task 89

--- src/main/kotlin/g0001_0100/s0089_gray_code/Solution.kt
+++ src/main/kotlin/g0001_0100/s0089_gray_code/Solution.kt
@@ -5,18 +5,18 @@ package g0001_0100.s0089_gray_code
 
 @Suppress("NAME_SHADOWING")
 class Solution {
-    fun grayCode(n: Int): List<Int?> {
+    fun grayCode(n: Int): List<Int> {
         var n = n
-        var n1 = arrayOf<Int?>(0)
+        var n1 = arrayOf(0)
         var shift = 1
         while (n > 0) {
-            val temp = arrayOfNulls<Int>(n1.size * 2)
+            val temp = Array(n1.size * 2) {0}
             var pos = 0
             for (integer in n1) {
                 temp[pos++] = integer
             }
             for (i in n1.indices.reversed()) {
-                temp[pos++] = n1[i]!! or shift
+                temp[pos++] = n1[i] or shift
             }
             n1 = temp
             shift = shift shl 1
