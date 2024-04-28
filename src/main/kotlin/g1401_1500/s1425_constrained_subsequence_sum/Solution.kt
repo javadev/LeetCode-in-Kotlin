21acc3f7b07d3716886cd2e09a6892f5ef0edda3
Valentyn Kolesnikov <javadev75@gmail.com>
2024-04-28 20:39:14 +0300

    Improved tasks

--- src/main/kotlin/g1401_1500/s1425_constrained_subsequence_sum/Solution.kt
+++ src/main/kotlin/g1401_1500/s1425_constrained_subsequence_sum/Solution.kt
@@ -3,23 +3,21 @@ package g1401_1500.s1425_constrained_subsequence_sum
 // #Hard #Array #Dynamic_Programming #Heap_Priority_Queue #Sliding_Window #Queue #Monotonic_Queue
 // #2023_06_07_Time_649_ms_(33.33%)_Space_51.4_MB_(100.00%)
 
-import java.util.LinkedList
-
 class Solution {
     fun constrainedSubsetSum(nums: IntArray, k: Int): Int {
         val n = nums.size
         var res = Int.MIN_VALUE
-        val mono = LinkedList<IntArray>()
+        val mono = ArrayList<IntArray>()
         for (i in 0 until n) {
             var take = nums[i]
-            while (mono.isNotEmpty() && i - mono.first[0] > k) {
+            while (mono.isNotEmpty() && i - mono.first()[0] > k) {
                 mono.removeFirst()
             }
             if (mono.isNotEmpty()) {
-                val mx = Math.max(0, mono.first[1])
+                val mx = Math.max(0, mono.first()[1])
                 take += mx
             }
-            while (mono.isNotEmpty() && take > mono.last[1]) {
+            while (mono.isNotEmpty() && take > mono.last()[1]) {
                 mono.removeLast()
             }
             mono.add(intArrayOf(i, take))
