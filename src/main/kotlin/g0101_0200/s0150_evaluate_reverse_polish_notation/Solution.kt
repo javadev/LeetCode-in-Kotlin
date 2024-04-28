7e2901136f9069165d1ab10d02d7ff3219f2f38a
Valentyn Kolesnikov <javadev75@gmail.com>
2024-04-28 03:15:50 -0400

    Improved Node and task 150

--- src/main/kotlin/g0101_0200/s0150_evaluate_reverse_polish_notation/Solution.kt
+++ src/main/kotlin/g0101_0200/s0150_evaluate_reverse_polish_notation/Solution.kt
@@ -3,14 +3,12 @@ package g0101_0200.s0150_evaluate_reverse_polish_notation
 // #Medium #Top_Interview_Questions #Array #Math #Stack #Programming_Skills_II_Day_3
 // #2022_10_09_Time_233_ms_(88.82%)_Space_36.7_MB_(91.45%)
 
-import java.util.function.BiFunction
-
 class Solution {
-    val op = mapOf<String, BiFunction<Int, Int, Int>>(
-        "/" to BiFunction { a, b -> a / b },
-        "*" to BiFunction { a, b -> a * b },
-        "+" to BiFunction { a, b -> a + b },
-        "-" to BiFunction { a, b -> a - b }
+    val op = mapOf<String, (Int, Int) -> Int>(
+        "/" to { a, b -> a / b },
+        "*" to { a, b -> a * b },
+        "+" to { a, b -> a + b },
+        "-" to { a, b -> a - b }
     )
     fun evalRPN(tokens: Array<String>): Int {
         val stack = ArrayDeque<String>()
@@ -18,7 +16,7 @@ class Solution {
             if (op.contains(t)) {
                 val b = stack.removeFirst().toInt()
                 val a = stack.removeFirst().toInt()
-                val c = op.getValue(t).apply(a, b)
+                val c = op.getValue(t).invoke(a, b)
                 stack.addFirst(c.toString())
             } else {
                 stack.addFirst(t)
