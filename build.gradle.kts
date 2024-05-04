9ed3f32a9bc6229025e0c693461af24f6f4dd7ef
Valentyn Kolesnikov <javadev75@gmail.com>
2024-05-04 07:48:10 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,9 @@ spotless {
     kotlin {
         encoding("UTF-8")
         target("**/src/**/*.kt")
-        ktlint("1.2.1")
+        ktlint("0.43.2").userData(mapOf(
+                "max_line_length" to "120"
+                ))
         toggleOffOn()
         trimTrailingWhitespace()
         endWithNewline()
