a83fab18356d57cfe0b2671def95c8ea6968cbbf
Valentyn Kolesnikov <javadev75@gmail.com>
2024-12-18 14:21:29 +0200

    Improved task 3389

--- src/main/kotlin/g3301_3400/s3389_minimum_operations_to_make_character_frequencies_equal/Solution.kt
+++ src/main/kotlin/g3301_3400/s3389_minimum_operations_to_make_character_frequencies_equal/Solution.kt
@@ -3,6 +3,10 @@ package g3301_3400.s3389_minimum_operations_to_make_character_frequencies_equal
 // #Hard #String #Hash_Table #Dynamic_Programming #Counting #Enumeration
 // #2024_12_18_Time_9_ms_(78.95%)_Space_39.3_MB_(18.42%)
 
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
 class Solution {
     fun makeStringGood(s: String): Int {
         val n = s.length
@@ -12,8 +16,8 @@ class Solution {
         var mx = 0
         for (c in cnt)
             if (c != 0) {
-                mn = Math.min(mn, c)
-                mx = Math.max(mx, c)
+                mn = min(mn, c)
+                mx = max(mx, c)
             }
         if (mn == mx) return 0
         var dp0 = 0
@@ -23,21 +27,21 @@ class Solution {
         var ans = n - 1
         for (i in mn..mx) {
             dp0 = cnt[0]
-            dp1 = Math.abs(i - cnt[0])
+            dp1 = abs(i - cnt[0])
             for (j in 1 until 26) {
                 tmp0 = dp0
                 tmp1 = dp1
-                dp0 = Math.min(tmp0, tmp1) + cnt[j]
-                if (cnt[j] >= i) {
-                    dp1 = Math.min(tmp0, tmp1) + (cnt[j] - i)
+                dp0 = min(tmp0, tmp1) + cnt[j]
+                dp1 = if (cnt[j] >= i) {
+                    min(tmp0, tmp1) + (cnt[j] - i)
                 } else {
-                    dp1 = Math.min(
-                        tmp0 + i - Math.min(i, cnt[j] + cnt[j - 1]),
-                        tmp1 + i - Math.min(i, cnt[j] + Math.max(0, cnt[j - 1] - i)),
+                    min(
+                        tmp0 + i - min(i, cnt[j] + cnt[j - 1]),
+                        tmp1 + i - min(i, cnt[j] + max(0, cnt[j - 1] - i)),
                     )
                 }
             }
-            ans = Math.min(ans, minOf(dp0, dp1))
+            ans = min(ans, minOf(dp0, dp1))
         }
         return ans
     }
