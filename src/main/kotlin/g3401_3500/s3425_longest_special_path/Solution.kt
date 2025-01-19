7fa8820429e38dc72d3657b0707d7594320ac4fd
Valentyn Kolesnikov <javadev75@gmail.com>
2025-01-19 16:09:06 +0200

    Improved task

--- src/main/kotlin/g3401_3500/s3425_longest_special_path/Solution.kt
+++ src/main/kotlin/g3401_3500/s3425_longest_special_path/Solution.kt
@@ -3,19 +3,19 @@ package g3401_3500.s3425_longest_special_path
 // #Hard #2025_01_19_Time_106_(100.00%)_Space_187.68_(100.00%)
 
 class Solution {
-    private lateinit var adj: Array<ArrayList<IntArray>?>
+    private lateinit var adj: Array<ArrayList<IntArray>>
     private lateinit var nums: IntArray
     private lateinit var dist: IntArray
     private lateinit var lastOccur: IntArray
-    private var pathStack: ArrayList<Int?>? = null
+    private lateinit var pathStack: ArrayList<Int>
     private var minIndex = 0
     private var maxLen = 0
     private var minNodesForMaxLen = 0
 
-    fun longestSpecialPath(edges: Array<IntArray>, nums: IntArray): IntArray? {
+    fun longestSpecialPath(edges: Array<IntArray>, nums: IntArray): IntArray {
         val n = nums.size
         this.nums = nums
-        adj = Array<ArrayList<IntArray>?>(n) { ArrayList<IntArray>() }
+        adj = Array<ArrayList<IntArray>>(n) { ArrayList<IntArray>() }
         for (i in 0..<n) {
             adj[i] = ArrayList<IntArray>()
         }
@@ -23,8 +23,8 @@ class Solution {
             val u = e[0]
             val v = e[1]
             val w = e[2]
-            adj[u]!!.add(intArrayOf(v, w))
-            adj[v]!!.add(intArrayOf(u, w))
+            adj[u].add(intArrayOf(v, w))
+            adj[v].add(intArrayOf(u, w))
         }
         dist = IntArray(n)
         buildDist(0, -1, 0)
@@ -36,7 +36,7 @@ class Solution {
         }
         lastOccur = IntArray(maxVal + 1)
         lastOccur.fill(-1)
-        pathStack = ArrayList<Int?>()
+        pathStack = ArrayList<Int>()
         minIndex = 0
         maxLen = 0
         minNodesForMaxLen = Int.Companion.MAX_VALUE
@@ -46,7 +46,7 @@ class Solution {
 
     private fun buildDist(u: Int, parent: Int, currDist: Int) {
         dist[u] = currDist
-        for (edge in adj[u]!!) {
+        for (edge in adj[u]) {
             val v = edge[0]
             val w = edge[1]
             if (v == parent) {
@@ -57,8 +57,8 @@ class Solution {
     }
 
     private fun dfs(u: Int, parent: Int) {
-        val stackPos = pathStack!!.size
-        pathStack!!.add(u)
+        val stackPos = pathStack.size
+        pathStack.add(u)
         val `val` = nums[u]
         val oldPos = lastOccur[`val`]
         val oldMinIndex = minIndex
@@ -67,7 +67,7 @@ class Solution {
             minIndex = oldPos + 1
         }
         if (minIndex <= stackPos) {
-            val ancestor = pathStack!![minIndex]!!
+            val ancestor = pathStack[minIndex]
             val pathLength = dist[u] - dist[ancestor]
             val pathNodes = stackPos - minIndex + 1
             if (pathLength > maxLen) {
@@ -77,14 +77,14 @@ class Solution {
                 minNodesForMaxLen = pathNodes
             }
         }
-        for (edge in adj[u]!!) {
+        for (edge in adj[u]) {
             val v = edge[0]
             if (v == parent) {
                 continue
             }
             dfs(v, u)
         }
-        pathStack!!.removeAt(pathStack!!.size - 1)
+        pathStack.removeAt(pathStack.size - 1)
         lastOccur[`val`] = oldPos
         minIndex = oldMinIndex
     }
