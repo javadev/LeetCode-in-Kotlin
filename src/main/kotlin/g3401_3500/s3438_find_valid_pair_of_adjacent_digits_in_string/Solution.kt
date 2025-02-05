f0bada0859bc0042c2798d17130e209818e7a3e3
Valentyn Kolesnikov <javadev75@gmail.com>
2025-02-05 04:05:57 +0200

    Improved task 3438

--- src/main/kotlin/g3401_3500/s3438_find_valid_pair_of_adjacent_digits_in_string/Solution.kt
+++ src/main/kotlin/g3401_3500/s3438_find_valid_pair_of_adjacent_digits_in_string/Solution.kt
@@ -1,11 +1,10 @@
 package g3401_3500.s3438_find_valid_pair_of_adjacent_digits_in_string
 
-// #Easy #String #Hash_Table #Counting #2025_02_05_Time_10_(40.91%)_Space_38.54_(29.55%)
+// #Easy #String #Hash_Table #Counting #2025_02_05_Time_2_(93.18%)_Space_36.38_(100.00%)
 
 class Solution {
     fun findValidPair(s: String): String {
         val t = IntArray(26)
-        t.fill(0)
         for (i in 0..<s.length) {
             t[s[i].code - '0'.code]++
         }
