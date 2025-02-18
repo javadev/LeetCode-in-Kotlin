8be1f25844e8ea5f20c973cbb3a2958b1d4a555e
Valentyn Kolesnikov <javadev75@gmail.com>
2025-02-18 04:20:17 +0200

    Improved 3453

--- src/main/kotlin/g3401_3500/s3453_separate_squares_i/Solution.kt
+++ src/main/kotlin/g3401_3500/s3453_separate_squares_i/Solution.kt
@@ -1,42 +1,58 @@
 package g3401_3500.s3453_separate_squares_i
 
-// #Medium #Array #Binary_Search #2025_02_18_Time_134_ms_(30.30%)_Space_95.92_MB_(93.94%)
+// #Medium #Array #Binary_Search #2025_02_18_Time_57_ms_(100.00%)_Space_102.84_MB_(84.85%)
 
 class Solution {
-    private fun helper(line: Double, squares: Array<IntArray>): Double {
-        var aAbove = 0.0
-        var aBelow = 0.0
-        for (square in squares) {
-            val y = square[1]
-            val l = square[2]
-            val total = l.toDouble() * l
-            if (line <= y) {
-                aAbove += total
-            } else if (line >= y + l) {
-                aBelow += total
-            } else {
-                // The line intersects the square.
-                val aboveHeight = (y + l) - line
-                val belowHeight = line - y
-                aAbove += l * aboveHeight
-                aBelow += l * belowHeight
-            }
-        }
-        return aAbove - aBelow
-    }
-
     fun separateSquares(squares: Array<IntArray>): Double {
-        var lo = 0.0
-        var hi = 2 * 1e9
-        for (i in 0..59) {
-            val mid = (lo + hi) / 2.0
-            val diff = helper(mid, squares)
-            if (diff > 0) {
-                lo = mid
+        val n = squares.size
+        val arr = Array(n) { LongArray(3) }
+        var total = 0.0
+        var left = Long.MAX_VALUE
+        var right = Long.MIN_VALUE
+        for (i in 0..n - 1) {
+            val y = squares[i][1].toLong()
+            val z = squares[i][2].toLong()
+            arr[i][0] = y
+            arr[i][1] = y + z
+            arr[i][2] = z
+            total += (z * z).toDouble()
+            left = minOf(left, arr[i][0])
+            right = maxOf(right, arr[i][1])
+        }
+        while (left < right) {
+            val mid = (left + right) / 2
+            var low = 0.0
+            for (a in arr) {
+                if (a[0] >= mid) {
+                    continue
+                } else if (a[1] <= mid) {
+                    low += a[2] * a[2]
+                } else {
+                    low += a[2] * (mid - a[0])
+                }
+            }
+            if (low + low + 0.00001 >= total) {
+                right = mid
             } else {
-                hi = mid
+                left = mid + 1
             }
         }
-        return hi
+        left = right - 1
+        var a1 = 0.0
+        var a2 = 0.0
+        for (a in arr) {
+            val x = a[0]
+            val y = a[1]
+            val z = a[2]
+            if (left > x) {
+                a1 += (minOf(y, left) - x) * z.toDouble()
+            }
+            if (right < y) {
+                a2 += (y - maxOf(x, right)) * z.toDouble()
+            }
+        }
+        val goal = (total - a1 - a1) / 2
+        val len = total - a1 - a2
+        return right - 1 + (goal / len)
     }
 }
