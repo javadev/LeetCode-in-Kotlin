923a4c65c5d3f958d5b0577c7887084aaedd92b9
Valentyn Kolesnikov <javadev75@gmail.com>
2025-02-25 04:00:46 +0200

    Improved task

--- src/main/kotlin/g3401_3500/s3462_maximum_sum_with_at_most_k_elements/Solution.kt
+++ src/main/kotlin/g3401_3500/s3462_maximum_sum_with_at_most_k_elements/Solution.kt
@@ -1,35 +1,29 @@
 package g3401_3500.s3462_maximum_sum_with_at_most_k_elements
 
 // #Medium #Array #Sorting #Greedy #Matrix #Heap_(Priority_Queue)
-// #2025_02_25_Time_197_ms_(86.21%)_Space_106.34_MB_(6.90%)
-
-import java.util.Collections
-import java.util.PriorityQueue
+// #2025_02_25_Time_139_ms_(100.00%)_Space_88.84_MB_(79.31%)
 
 class Solution {
     fun maxSum(grid: Array<IntArray>, limits: IntArray, k: Int): Long {
-        if (grid.isEmpty()) {
-            return 0
+        var l = 0
+        for (i in limits.indices) {
+            l += limits[i]
         }
-        val pq = PriorityQueue<Int>(Collections.reverseOrder<Int>())
-        var temp: PriorityQueue<Int>
+        val dp = IntArray(l)
+        var a = 0
         for (i in grid.indices) {
-            temp = PriorityQueue<Int>(Collections.reverseOrder<Int>())
-            for (j in grid[i].indices) {
-                temp.add(grid[i][j])
-            }
-            var cnt = 0
-            while (temp.isNotEmpty() && cnt < limits[i]) {
-                pq.add(temp.poll())
-                cnt += 1
+            val lim = limits[i]
+            grid[i].sort()
+            for (j in grid[i].size - lim..<grid[i].size) {
+                dp[a] = grid[i][j]
+                a++
             }
         }
-        var result: Long = 0
-        var count: Long = 0
-        while (pq.isNotEmpty() && count < k) {
-            result += pq.poll()!!.toLong()
-            count += 1
+        dp.sort()
+        var sum = 0L
+        for (i in l - 1 downTo l - k) {
+            sum += dp[i].toLong()
         }
-        return result
+        return sum
     }
 }
