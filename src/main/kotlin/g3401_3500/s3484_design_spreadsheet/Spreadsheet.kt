506655d00552020c034084c8654036f2361aacba
Valentyn Kolesnikov <javadev75@gmail.com>
2025-03-16 16:34:00 +0200

    Added tasks 3483-3486

---
+++ src/main/kotlin/g3401_3500/s3484_design_spreadsheet/Spreadsheet.kt
@@ -0,0 +1,43 @@
+package g3401_3500.s3484_design_spreadsheet
+
+// #Medium #2025_03_16_Time_176_ms_(100.00%)_Space_79.62_MB_(100.00%)
+
+@Suppress("unused")
+class Spreadsheet(rows: Int) {
+    private val data: MutableMap<String, Int> = HashMap<String, Int>()
+
+    fun setCell(cell: String, value: Int) {
+        data.put(cell, value)
+    }
+
+    fun resetCell(cell: String) {
+        data.put(cell, 0)
+    }
+
+    fun getValue(formula: String): Int {
+        val index = formula.indexOf('+')
+        val left = formula.substring(1, index)
+        val right = formula.substring(index + 1)
+        val x =
+            if (Character.isLetter(left[0])) {
+                data.getOrDefault(left, 0)
+            } else {
+                left.toInt()
+            }
+        val y =
+            if (Character.isLetter(right[0])) {
+                data.getOrDefault(right, 0)
+            } else {
+                right.toInt()
+            }
+        return x + y
+    }
+}
+
+/*
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet obj = new Spreadsheet(rows);
+ * obj.setCell(cell,value);
+ * obj.resetCell(cell);
+ * int param_3 = obj.getValue(formula);
+ */
