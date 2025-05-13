ae152001fd8dd4fdb5cc3d0c6ac94e635cebef24
Valentyn Kolesnikov <javadev75@gmail.com>
2025-05-13 06:34:25 +0300

    Added tests

--- src/test/kotlin/g3501_3600/s3543_maximum_weighted_k_edge_path/SolutionTest.kt
+++ src/test/kotlin/g3501_3600/s3543_maximum_weighted_k_edge_path/SolutionTest.kt
@@ -7,33 +7,74 @@ import org.junit.jupiter.api.Test
 internal class SolutionTest {
     @Test
     fun maxWeight() {
-        assertThat<Int>(
+        assertThat(
             Solution().maxWeight(3, arrayOf<IntArray>(intArrayOf(0, 1, 1), intArrayOf(1, 2, 2)), 2, 4),
-            equalTo<Int>(3),
+            equalTo(3),
         )
     }
 
     @Test
     fun maxWeight2() {
-        assertThat<Int>(
+        assertThat(
             Solution().maxWeight(3, arrayOf<IntArray>(intArrayOf(0, 1, 2), intArrayOf(0, 2, 3)), 1, 3),
-            equalTo<Int>(2),
+            equalTo(2),
         )
     }
 
     @Test
     fun maxWeight3() {
-        assertThat<Int>(
+        assertThat(
             Solution().maxWeight(3, arrayOf<IntArray>(intArrayOf(0, 1, 6), intArrayOf(1, 2, 8)), 1, 6),
-            equalTo<Int>(-1),
+            equalTo(-1),
         )
     }
 
     @Test
     fun maxWeight4() {
-        assertThat<Int>(
+        assertThat(
             Solution().maxWeight(3, arrayOf<IntArray>(intArrayOf(0, 1, 6), intArrayOf(1, 2, 8)), 0, 6),
-            equalTo<Int>(0),
+            equalTo(0),
+        )
+    }
+
+    @Test
+    fun maxWeight5() {
+        assertThat(
+            Solution()
+                .maxWeight(
+                    6,
+                    arrayOf<IntArray>(
+                        intArrayOf(0, 1, 10),
+                        intArrayOf(0, 2, 1),
+                        intArrayOf(1, 3, 2),
+                        intArrayOf(2, 3, 5),
+                        intArrayOf(3, 4, 5),
+                        intArrayOf(3, 5, 3),
+                    ),
+                    3,
+                    12,
+                ),
+            equalTo(11),
+        )
+    }
+
+    @Test
+    fun maxWeight6() {
+        assertThat(
+            Solution()
+                .maxWeight(
+                    5,
+                    arrayOf<IntArray>(
+                        intArrayOf(0, 1, 2),
+                        intArrayOf(0, 2, 3),
+                        intArrayOf(1, 3, 3),
+                        intArrayOf(2, 3, 1),
+                        intArrayOf(3, 4, 2),
+                    ),
+                    3,
+                    7,
+                ),
+            equalTo(6),
         )
     }
 }
