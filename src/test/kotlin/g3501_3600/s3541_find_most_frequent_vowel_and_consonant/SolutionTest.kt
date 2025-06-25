e2eca6ca369d7f39edd382e1e05e91c4152a265c
Valentyn Kolesnikov <javadev75@gmail.com>
2025-06-25 11:32:43 +0300

    Improved task 3541

--- src/test/kotlin/g3501_3600/s3541_find_most_frequent_vowel_and_consonant/SolutionTest.kt
+++ src/test/kotlin/g3501_3600/s3541_find_most_frequent_vowel_and_consonant/SolutionTest.kt
@@ -1,17 +1,17 @@
 package g3501_3600.s3541_find_most_frequent_vowel_and_consonant
 
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 
 internal class SolutionTest {
     @Test
     fun maxFreqSum() {
-        MatcherAssert.assertThat<Int?>(Solution().maxFreqSum("successes"), CoreMatchers.equalTo<Int?>(6))
+        assertThat<Int>(Solution().maxFreqSum("successes"), equalTo<Int>(6))
     }
 
     @Test
     fun maxFreqSum2() {
-        MatcherAssert.assertThat<Int?>(Solution().maxFreqSum("aeiaeia"), CoreMatchers.equalTo<Int?>(3))
+        assertThat<Int>(Solution().maxFreqSum("aeiaeia"), equalTo<Int>(3))
     }
 }
