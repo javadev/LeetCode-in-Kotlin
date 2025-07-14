2b12fe61b9ba0c58a147199657b612ecbd4d02a4
Valentyn Kolesnikov <javadev75@gmail.com>
2025-07-14 10:04:49 +0300

    Improved tasks 1-5

--- src/main/kotlin/g0001_0100/s0001_two_sum/Solution.kt
+++ src/main/kotlin/g0001_0100/s0001_two_sum/Solution.kt
@@ -3,11 +3,11 @@ package g0001_0100.s0001_two_sum
 // #Easy #Top_100_Liked_Questions #Top_Interview_Questions #Array #Hash_Table
 // #Data_Structure_I_Day_2_Array #Level_1_Day_13_Hashmap #Udemy_Arrays #Top_Interview_150_Hashmap
 // #Big_O_Time_O(n)_Space_O(n) #AI_can_be_used_to_solve_the_task
-// #2023_07_03_Time_202_ms_(91.18%)_Space_38.1_MB_(76.07%)
+// #2025_07_11_Time_2_ms_(98.77%)_Space_48.00_MB_(52.59%)
 
 class Solution {
     fun twoSum(numbers: IntArray, target: Int): IntArray {
-        val indexMap: MutableMap<Int, Int> = HashMap()
+        val indexMap = HashMap<Int, Int>()
         for (i in numbers.indices) {
             val requiredNum = target - numbers[i]
             if (indexMap.containsKey(requiredNum)) {
--- src/main/kotlin/g0001_0100/s0004_median_of_two_sorted_arrays/Solution.kt
+++ src/main/kotlin/g0001_0100/s0004_median_of_two_sorted_arrays/Solution.kt
@@ -2,25 +2,39 @@ package g0001_0100.s0004_median_of_two_sorted_arrays
 
 // #Hard #Top_100_Liked_Questions #Top_Interview_Questions #Array #Binary_Search #Divide_and_Conquer
 // #Top_Interview_150_Binary_Search #Big_O_Time_O(log(min(N,M)))_Space_O(1)
-// #AI_can_be_used_to_solve_the_task #2023_07_03_Time_293_ms_(75.96%)_Space_47.5_MB_(64.85%)
+// #AI_can_be_used_to_solve_the_task #2025_07_11_Time_2_ms_(99.23%)_Space_51.04_MB_(73.69%)
+
+import kotlin.math.max
+import kotlin.math.min
 
 class Solution {
     fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
-        val l: MutableList<Int> = ArrayList()
-        val f: Double
-        for (j in nums1) {
-            l.add(j)
+        if (nums2.size < nums1.size) {
+            return findMedianSortedArrays(nums2, nums1)
         }
-        for (i in nums2) {
-            l.add(i)
+        val n1 = nums1.size
+        val n2 = nums2.size
+        var low = 0
+        var high = n1
+        while (low <= high) {
+            val cut1 = (low + high) / 2
+            val cut2 = ((n1 + n2 + 1) / 2) - cut1
+            val l1 = if (cut1 == 0) Int.MIN_VALUE else nums1[cut1 - 1]
+            val l2 = if (cut2 == 0) Int.MIN_VALUE else nums2[cut2 - 1]
+            val r1 = if (cut1 == n1) Int.MAX_VALUE else nums1[cut1]
+            val r2 = if (cut2 == n2) Int.MAX_VALUE else nums2[cut2]
+            if (l1 <= r2 && l2 <= r1) {
+                return if ((n1 + n2) % 2 == 0) {
+                    (max(l1, l2).toDouble() + min(r1, r2).toDouble()) / 2.0
+                } else {
+                    max(l1, l2).toDouble()
+                }
+            } else if (l1 > r2) {
+                high = cut1 - 1
+            } else {
+                low = cut1 + 1
+            }
         }
-        l.sort()
-        val k = l.size
-        f = if (k % 2 == 0) {
-            (l[k / 2 - 1] + l[k / 2]).toDouble() / 2
-        } else {
-            l[(k + 1) / 2 - 1].toDouble()
-        }
-        return f
+        return 0.0
     }
 }
