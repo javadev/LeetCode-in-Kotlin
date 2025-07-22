bb676bfdb0adf70ea954169280e0921977be1083
Valentyn Kolesnikov <javadev75@gmail.com>
2025-07-22 22:45:11 +0300

    Improved task 3620

--- src/main/kotlin/g3601_3700/s3620_network_recovery_pathways/Solution.kt
+++ src/main/kotlin/g3601_3700/s3620_network_recovery_pathways/Solution.kt
@@ -22,7 +22,7 @@ class Solution {
                 q.offer(i)
             }
         }
-        while (!q.isEmpty()) {
+        while (q.isNotEmpty()) {
             val u = q.poll()
             ts.add(u)
             for (v in g[u]) {
