e061338318357f459442e75a55e17ed3bddd3f42
Valentyn Kolesnikov <javadev75@gmail.com>
2025-08-14 08:33:43 +0300

    Improved task 3637

--- src/main/kotlin/g3601_3700/s3637_trionic_array_i/Solution.kt
+++ src/main/kotlin/g3601_3700/s3637_trionic_array_i/Solution.kt
@@ -1,29 +1,26 @@
 package g3601_3700.s3637_trionic_array_i
 
-// #Easy #Array #Weekly_Contest_461 #2025_08_03_Time_1_ms_(100.00%)_Space_43.69_MB_(100.00%)
+// #Easy #Array #Weekly_Contest_461 #2025_08_14_Time_0_ms_(100.00%)_Space_43.15_MB_(44.56%)
 
 class Solution {
     fun isTrionic(nums: IntArray): Boolean {
-        var p = 0
-        var q = 0
+        var i = 1
         val n = nums.size
-        for (i in 1..<n - 1) {
-            if (nums[i - 1] == nums[i]) {
-                return false
-            }
-            if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]) {
-                if (p != 0) {
-                    return false
-                }
-                p = i
-            }
-            if (nums[i - 1] > nums[i] && nums[i] < nums[i + 1]) {
-                if (p == 0 || q != 0) {
-                    return false
-                }
-                q = i
-            }
+        while (i < n && nums[i] > nums[i - 1]) {
+            i++
         }
-        return q > 0
+        if (i == n || i == 1) {
+            return false
+        }
+        while (i < n && nums[i] < nums[i - 1]) {
+            i++
+        }
+        if (i == n) {
+            return false
+        }
+        while (i < n && nums[i] > nums[i - 1]) {
+            i++
+        }
+        return i == n
     }
 }
