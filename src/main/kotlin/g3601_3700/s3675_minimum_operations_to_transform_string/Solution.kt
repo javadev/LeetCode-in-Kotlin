ad646a8a96e34e5bb5abcda1351ad1842aea408f
Valentyn Kolesnikov <javadev75@gmail.com>
2025-09-14 12:23:09 +0300

    Update Solution.kt

--- src/main/kotlin/g3601_3700/s3675_minimum_operations_to_transform_string/Solution.kt
+++ src/main/kotlin/g3601_3700/s3675_minimum_operations_to_transform_string/Solution.kt
@@ -7,7 +7,7 @@ class Solution {
         val n = s.length
         var ans = 0
         for (i in 0..<n) {
-            val c = s.get(i)
+            val c = s[i]
             if (c != 'a') {
                 val ops = 'z'.code - c.code + 1
                 if (ops > ans) {
