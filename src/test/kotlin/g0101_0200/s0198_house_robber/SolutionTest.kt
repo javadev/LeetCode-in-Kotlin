b2cce579e98cd4a95b72e506ec5a886ef0d07c33
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2025-11-28 09:20:12 +0700

    Added tests for tasks 101-2929

--- src/test/kotlin/g0101_0200/s0198_house_robber/SolutionTest.kt
+++ src/test/kotlin/g0101_0200/s0198_house_robber/SolutionTest.kt
@@ -29,4 +29,34 @@ internal class SolutionTest {
     fun rob5() {
         assertThat(Solution().rob(intArrayOf(1, 2)), equalTo(2))
     }
+
+    @Test
+    fun rob6() {
+        assertThat(Solution().rob(intArrayOf(5)), equalTo(5))
+    }
+
+    @Test
+    fun rob7() {
+        assertThat(Solution().rob(intArrayOf(5, 10)), equalTo(10))
+    }
+
+    @Test
+    fun rob8() {
+        assertThat(Solution().rob(intArrayOf(2, 1, 1, 2)), equalTo(4))
+    }
+
+    @Test
+    fun rob9() {
+        assertThat(Solution().rob(intArrayOf(3, 3, 3, 3, 3)), equalTo(9))
+    }
+
+    @Test
+    fun rob10() {
+        assertThat(Solution().rob(intArrayOf(100, 1, 100, 1, 100)), equalTo(300))
+    }
+
+    @Test
+    fun rob11() {
+        assertThat(Solution().rob(intArrayOf()), equalTo(0))
+    }
 }
--- src/test/kotlin/g2901_3000/s2929_distribute_candies_among_children_ii/SolutionTest.kt
+++ src/test/kotlin/g2901_3000/s2929_distribute_candies_among_children_ii/SolutionTest.kt
@@ -14,4 +14,39 @@ internal class SolutionTest {
     fun distributeCandies2() {
         assertThat(Solution().distributeCandies(3, 3), equalTo(10L))
     }
+
+    @Test
+    fun distributeCandies3() {
+        assertThat(Solution().distributeCandies(2, 5), equalTo(6L))
+    }
+
+    @Test
+    fun distributeCandies4() {
+        assertThat(Solution().distributeCandies(4, 4), equalTo(15L))
+    }
+
+    @Test
+    fun distributeCandies5() {
+        assertThat(Solution().distributeCandies(7, 5), equalTo(27L))
+    }
+
+    @Test
+    fun distributeCandies6() {
+        assertThat(Solution().distributeCandies(12, 5), equalTo(10L))
+    }
+
+    @Test
+    fun distributeCandies7() {
+        assertThat(Solution().distributeCandies(20, 5), equalTo(0L))
+    }
+
+    @Test
+    fun distributeCandies8() {
+        assertThat(Solution().distributeCandies(10, 5), equalTo(21L))
+    }
+
+    @Test
+    fun distributeCandies9() {
+        assertThat(Solution().distributeCandies(15, 5), equalTo(1L))
+    }
 }
