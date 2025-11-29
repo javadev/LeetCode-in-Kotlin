fe64656b65433c0e6669163267a671057c15897b
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2025-11-29 11:15:22 +0700

    Added tests for tasks 564-2420

--- src/test/kotlin/g1101_1200/s1154_day_of_the_year/SolutionTest.kt
+++ src/test/kotlin/g1101_1200/s1154_day_of_the_year/SolutionTest.kt
@@ -14,4 +14,54 @@ internal class SolutionTest {
     fun dayOfYear2() {
         assertThat(Solution().dayOfYear("2019-02-10"), equalTo(41))
     }
+
+    @Test
+    fun dayOfYear3() {
+        assertThat(Solution().dayOfYear("2020-02-01"), equalTo(32))
+    }
+
+    @Test
+    fun dayOfYear4() {
+        assertThat(Solution().dayOfYear("2020-03-01"), equalTo(61))
+    }
+
+    @Test
+    fun dayOfYear5() {
+        assertThat(Solution().dayOfYear("2019-02-28"), equalTo(59))
+    }
+
+    @Test
+    fun dayOfYear6() {
+        assertThat(Solution().dayOfYear("2020-02-29"), equalTo(60))
+    }
+
+    @Test
+    fun dayOfYear7() {
+        assertThat(Solution().dayOfYear("2019-12-31"), equalTo(365))
+    }
+
+    @Test
+    fun dayOfYear8() {
+        assertThat(Solution().dayOfYear("2020-12-31"), equalTo(366))
+    }
+
+    @Test
+    fun dayOfYear9() {
+        assertThat(Solution().dayOfYear("2021-01-01"), equalTo(1))
+    }
+
+    @Test
+    fun dayOfYear10() {
+        assertThat(Solution().dayOfYear("2021-07-04"), equalTo(185))
+    }
+
+    @Test
+    fun dayOfYear11() {
+        assertThat(Solution().dayOfYear("2000-02-29"), equalTo(60))
+    }
+
+    @Test
+    fun dayOfYear12() {
+        assertThat(Solution().dayOfYear("1900-03-01"), equalTo(60))
+    }
 }
--- src/test/kotlin/g2401_2500/s2420_find_all_good_indices/SolutionTest.kt
+++ src/test/kotlin/g2401_2500/s2420_find_all_good_indices/SolutionTest.kt
@@ -20,4 +20,68 @@ internal class SolutionTest {
             equalTo(emptyList<Any>()),
         )
     }
+
+    @Test
+    fun goodIndices3() {
+        assertThat(
+            Solution().goodIndices(intArrayOf(5, 3, 4, 2, 1), 1),
+            equalTo(listOf(1, 2, 3)),
+        )
+    }
+
+    @Test
+    fun goodIndices4() {
+        assertThat(
+            Solution().goodIndices(intArrayOf(5, 4, 3, 2, 1), 2),
+            equalTo(emptyList<Int>()),
+        )
+    }
+
+    @Test
+    fun goodIndices5() {
+        assertThat(
+            Solution().goodIndices(intArrayOf(1, 2, 3, 4, 5), 2),
+            equalTo(emptyList<Int>()),
+        )
+    }
+
+    @Test
+    fun goodIndices6() {
+        assertThat(
+            Solution().goodIndices(intArrayOf(1, 2), 2),
+            equalTo(emptyList<Int>()),
+        )
+    }
+
+    @Test
+    fun goodIndices7() {
+        assertThat(
+            Solution().goodIndices(intArrayOf(5, 4, 4, 3, 2, 2, 3, 4, 4, 3, 2), 2),
+            equalTo(listOf(3, 4, 5, 6)),
+        )
+    }
+
+    @Test
+    fun goodIndices8() {
+        assertThat(
+            Solution().goodIndices(intArrayOf(3, 2, 1, 2, 3, 2, 1, 2, 3), 2),
+            equalTo(listOf(2, 6)),
+        )
+    }
+
+    @Test
+    fun goodIndices9() {
+        assertThat(
+            Solution().goodIndices(intArrayOf(3, 2, 1, 1, 2, 3), 2),
+            equalTo(listOf(2, 3)),
+        )
+    }
+
+    @Test
+    fun goodIndices10() {
+        assertThat(
+            Solution().goodIndices(intArrayOf(2, 2, 2, 2, 2), 2),
+            equalTo(listOf(2)),
+        )
+    }
 }
