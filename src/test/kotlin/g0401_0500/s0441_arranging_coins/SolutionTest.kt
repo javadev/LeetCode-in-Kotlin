057d64bb33bc91e5e44e36d803475c463e34750e
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2025-11-29 11:13:53 +0700

    Added tests for tasks 441-3040

--- src/test/kotlin/g0401_0500/s0441_arranging_coins/SolutionTest.kt
+++ src/test/kotlin/g0401_0500/s0441_arranging_coins/SolutionTest.kt
@@ -14,4 +14,39 @@ internal class SolutionTest {
     fun arrangeCoins2() {
         assertThat(Solution().arrangeCoins(8), equalTo(3))
     }
+
+    @Test
+    fun arrangeCoins3() {
+        assertThat(Solution().arrangeCoins(0), equalTo(0))
+    }
+
+    @Test
+    fun arrangeCoins4() {
+        assertThat(Solution().arrangeCoins(1), equalTo(1))
+    }
+
+    @Test
+    fun arrangeCoins5() {
+        assertThat(Solution().arrangeCoins(2), equalTo(1))
+    }
+
+    @Test
+    fun arrangeCoins6() {
+        assertThat(Solution().arrangeCoins(3), equalTo(2))
+    }
+
+    @Test
+    fun arrangeCoins7() {
+        assertThat(Solution().arrangeCoins(6), equalTo(3))
+    }
+
+    @Test
+    fun arrangeCoins8() {
+        assertThat(Solution().arrangeCoins(2147483647), equalTo(65535))
+    }
+
+    @Test
+    fun arrangeCoins9() {
+        assertThat(Solution().arrangeCoins(7), equalTo(3))
+    }
 }
